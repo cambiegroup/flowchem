0d88d02469ddf2fc1468c4161143f1a05b1b4353
Dario Cambie <dario.cambie@mpikg.mpg.de>
2020-12-10 17:50:22 +0100

    Updated power supply test code

--- flowchem/examples/power_supply_test.py
+++ flowchem/examples/power_supply_test.py
@@ -4,21 +4,11 @@ import time
 
 def test():
 	ps = InstrumentInterface()
-	ps.open("com3")
+	ps.open("COM30")
 
-	input("SETTING PS TO 2V 0.1A, Enter to continue, auto-stop in 3 seconds...")
-	ps.set_current(0.1)
-	ps.set_voltage(2)
-	ps.output_on()
-	time.sleep(3)
-	output = ps.get_output_read()
-	ps.output_off()
-
-	print(f"V, I, MODE = {output}")
-
-	input("SETTING PS TO 10V 0.2A, Enter to continue, auto-stop in 3 seconds...")
-	ps.set_current(0.2)
-	ps.set_voltage(10)
+	input("SETTING PS TO 36V 2A, Enter to continue, auto-stop in 3 seconds...")
+	ps.set_current(0.4)
+	ps.set_voltage(31)
 	ps.output_on()
 	time.sleep(3)
 	output = ps.get_output_read()
