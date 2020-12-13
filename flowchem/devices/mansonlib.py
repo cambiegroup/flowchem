5bbde7b382f385eccc1c47330c76836460d98e82
Dario Cambié <dario.cambie@glasgow.ac.uk>
2020-12-13 02:23:43 +0100

    Streamline mansonlib code

--- flowchem/devices/mansonlib.py
+++ flowchem/devices/mansonlib.py
@@ -9,7 +9,7 @@ Changes:
 
 import re
 import operator
-from typing import Union, Literal, Tuple, Optional
+from typing import Union, Literal, Tuple, Optional, List
 import serial
 
 
@@ -22,6 +22,10 @@ class NotConnectedError(MansonException):
 
 
 class InvalidArgument(MansonException):
+    pass
+
+
+class InvalidOrNoReply(MansonException):
     pass
 
 
@@ -55,18 +59,29 @@ class InstrumentInterface:
             return True
         return False
 
-    def _send_command(self, command: str) -> str:
+    def _send_command(self, command: str, multiline_reply: bool = False) -> str:
         """ Internal function to send command and read reply. """
         if self.sp is None:
             raise NotConnectedError
+
+        # Flush buffer, write command and read reply
         self.sp.reset_input_buffer()
         self.sp.write(f"{command}\r".encode('ascii'))
-        return self.sp.readline().decode('ascii').strip()
+        response = self.sp.readline().decode('ascii').strip()
 
-    def get_info(self):
-        response = self._send_command("GMOD")
         if not response:
-            return False
+            raise InvalidOrNoReply
+
+        # Get multiple lines if needed
+        if multiline_reply:
+            while additional_response := self.sp.readline().decode('ascii').strip():
+                response += "\n" + additional_response
+
+        return response
+
+    def get_info(self) -> str:
+        """ Returns the model name of the connected device """
+        response = self._send_command("GMOD")
 
         pattern = re.compile(r'.*\d{4}\s')
         match = pattern.match(response)
@@ -75,27 +90,25 @@ class InstrumentInterface:
             if response[0:4] == "HCS-":
                 return match.group().rstrip()
             return "HCS-" + match.group().rstrip()
-        return False
+        raise InvalidOrNoReply
 
-    def output_on(self):
+    def output_on(self) -> bool:
         response = self._send_command("SOUT0")
         return response == "OK"
 
-    def output_off(self):
+    def output_off(self) -> bool:
         response = self._send_command("SOUT1")
         return response == "OK"
 
-    def get_output_read(self) -> Union[bool, Tuple[float, float, Literal["CC", "CV", False]]]:
+    def get_output_read(self) -> Tuple[float, float, Literal["CC", "CV", False]]:
         """ Returns actual values of voltage, current and mode """
         response = self._send_command("GETD")
-        if not response:
-            return False
 
         try:
             volt = float(response[0:4]) / 100
             curr = float(response[4:8]) / 100
         except ValueError:
-            return False
+            raise InvalidOrNoReply
 
         if response[8:9] == '0':
             mode = "CV"
@@ -120,19 +133,12 @@ class InstrumentInterface:
 
     def get_output_power(self) -> Optional[float]:
         """ Returns output power in watts """
-        if self.sp is not None:
-            try:
-                voltage, intensity, _ = self.get_output_read()
-                return voltage * intensity
-            except TypeError:
-                return False
-        return False
+        voltage, intensity, _ = self.get_output_read()
+        return voltage * intensity
 
-    def get_max(self) -> Union[bool, Tuple[float, float]]:
+    def get_max(self) -> Tuple[float, float]:
         """ Returns maximum voltage and current, as tuple, or False. """
         response = self._send_command("GMAX")
-        if not response:
-            return False
 
         max_v = int(response[0:3])
         max_c = int(response[3:6])
@@ -144,23 +150,21 @@ class InstrumentInterface:
             max_c /= 10
         return max_v, max_c / float(10)
 
-    def get_setting(self) -> Union[bool, Tuple[float, float]]:
+    def get_setting(self) -> Tuple[float, float]:
         """ Returns current setting as tuple (voltage, current). """
         response = self._send_command("GETS")
-        if not response:
-            return False
 
+        # RegEx to only keep numbers
         response = re.sub(r"\D", "", response)
         v_setting = float(response[0:3]) / 10
         c_setting = float(response[3:6])
 
-        model = self.get_info()
-        if model in self.MODEL_ALT_RANGE:
+        if self.get_info() in self.MODEL_ALT_RANGE:
             c_setting /= 10
 
         return v_setting, c_setting / 10
 
-    def set_voltage(self, value_in_volt: Union[int, float]):
+    def set_voltage(self, value_in_volt: Union[int, float]) -> bool:
         """ Set target voltage """
         # Zero fill by left pad with zeros, up to three digits
         cmd = "VOLT" + str(value_in_volt * 10).zfill(3)
@@ -168,138 +172,86 @@ class InstrumentInterface:
         response = self._send_command(cmd)
         return response == "OK"
 
-    def set_current(self, current_in_ampere):
+    def set_current(self, current_in_ampere) -> bool:
         if not isinstance(current_in_ampere, (int, float)):
             raise InvalidArgument
 
-        model = self.get_info()
-        if model in self.MODEL_ALT_RANGE:
+        if self.get_info() in self.MODEL_ALT_RANGE:
             cmd = "CURR" + str(current_in_ampere * 100).zfill(3)
             if current_in_ampere > 10:
-                return MansonException("Invalid current intensity for device!")
+                raise MansonException("Invalid current intensity for device!")
         else:
             cmd = "CURR" + str(current_in_ampere * 10).zfill(3)
             if current_in_ampere > 100:
-                return MansonException("Invalid current intensity for device!")
+                raise MansonException("Invalid current intensity for device!")
 
         response = self._send_command(cmd)
         return response == "OK"
 
-    def Pset(self, num: int, volt, curr):
-        if isinstance(num, int):
-            num_max = 3
-            if num > num_max - 1 or num < 0:
-                return False
-            for x in range(num_max):
-                if x == num:
-                    index = num + x
-                    break
-        model_list = ['HCS-3102', 'HCS-3014', 'HCS-3204']
-        list_c = ['3', '6', '10', '13', '17', '20']
-        response = self._send_command("GETM")
+    def set_all_preset(self, preset: List[Tuple[float, float]]) -> bool:
+        """ Set all 3 preset memory position with voltage/current values """
+        voltage_multiplier = 100 if self.get_info() in self.MODEL_ALT_RANGE else 10
+        command = "PROM"
 
-        if not response:
-            return False
-        response = list(response)
-        if isinstance(volt, (int, float)):
-            list_v = ['0', '3', '7', '10', '14', '17']
-            if 0 <= volt < 1:
-                volt = int(volt * 10)
-                response[(int(list_v[index])):(int(list_v[index + 1]))] = '00' + str(volt)
-            elif 1 <= volt < 10:
-                volt = int(volt * 10)
-                response[(int(list_v[index])):(int(list_v[index + 1]))] = '0' + str(volt)
-            elif 10 <= volt < 100:
-                volt = int(volt * 10)
-                response[(int(list_v[index])):(int(list_v[index + 1]))] = str(volt)
-            else:
-                return False
-        model = self.get_info()
-        model_mark = 0
-        if model == '':
-            return False
-        if isinstance(curr, (int, float)):
-            for model_ in model_list:
-                if operator.eq(model[0:8], model_) == 1:
-                    if 0 <= curr < 1:
-                        curr = int(curr * 100)
-                        response[(int(list_c[index])):(int(list_c[index + 1]))] = '0' + str(curr)
-                    elif 1 <= curr < 10:
-                        curr = int(curr * 100)
-                        response[(int(list_c[index])):(int(list_c[index + 1]))] = str(curr)
-                    else:
-                        return False
-                    model_mark = 1
-                    break
-        else:
-            return False
-        if model_mark == 0:
-            if 0 <= curr < 1:
-                curr = int(curr * 10)
-                response[(int(list_c[index])):(int(list_c[index + 1]))] = '00' + str(curr)
-            elif 1 <= curr < 10:
-                curr = int(curr * 10)
-                response[(int(list_c[index])):(int(list_c[index + 1]))] = '0' + str(curr)
-            elif 10 <= curr < 99:
-                curr = int(curr * 10)
-                response[(int(list_c[index])):(int(list_c[index + 1]))] = str(curr)
-            else:
-                return False
-        response = ''.join(response)
-        response = response[0:6] + response[7:13] + response[14:20]
-        cmd = 'PROM' + response
-        response = self._send_command(cmd)
-        if not response:
-            return False
-        return response
+        for set_values in preset:
+            voltage, current = set_values
+            voltage_string = str(int(voltage * voltage_multiplier))
+            current_string = str(int(current * 10))
+            command += voltage_string + current_string
 
-    def GPset(self, index, option):
-        model_list = ['HCS-3102', 'HCS-3014', 'HCS-3204']
-        model = self.get_info()
-        model_mark = 0
-        list_v = ['0', '3', '6', '9', '12', '15']
-        list_c = ['3', '6', '9', '12', '15', '18']
-        max_index = 3
-        if isinstance(index, (int, float)) and (
-            index < 0 or index > max_index - 1
-        ):
-            return False
-        for x in range(max_index):
-            if x == index:
-                index = index + x
-                break
-        response = self._send_command("GETM")
-        if not response:
-            return False
-        response = re.sub(r"\D", "", response)
-        if option == 'A':
-            volt = int(response[(int(list_v[index])):(int(list_v[index + 1]))]) / float(10)
-            for model__ in model_list:
-                if operator.eq(model[0:8], model__) == 1:
-                    curr = int(response[(int(list_c[index])):(int(list_c[index + 1]))]) / float(100)
-                    return str(volt) + " " + str(curr)
-            curr = int(response[(int(list_c[index])):(int(list_c[index + 1]))]) / float(10)
-            return str(volt) + " " + str(curr)
-        elif option == 'C':
-            for model_ in model_list:
-                if operator.eq(model[0:8], model_) == 1:
-                    curr = int(response[(int(list_c[index])):(int(list_c[index + 1]))]) / float(100)
-                    return curr
-            curr = int(response[(int(list_c[index])):(int(list_c[index + 1]))]) / float(10)
-            return curr
-        elif option == 'V':
-            volt = int(response[(int(list_v[index])):(int(list_v[index + 1]))]) / float(10)
-            return volt
+        response = self._send_command(command)
+        return response == "OK"
 
-    def RPreset(self, index: int) -> bool:
-        """ Reset one of the preset position: 0, 1 or 2 """
+    def set_preset(self, index: int, voltage: float, current: float) -> bool:
+        """ Set preset position index with the provided values of voltage and current """
+        preset = self.get_all_preset()
+        try:
+            preset[index] = (voltage, current)
+        except KeyError as e:
+            raise InvalidArgument from e
+        return self.set_all_preset(preset)
+
+    def get_all_preset(self) -> List[Tuple[float, float]]:
+        """ Get voltage and current for all 3 memory preset position """
+        response_lines = self._send_command("GETM", multiline_reply=True).split("\n")
+
+        voltage = []
+        current = []
+        for preset in response_lines[0:3]:
+            # Drop all but numbers
+            preset = re.sub(r"\D", "", preset)
+            try:
+                # Three digits for voltage and three digits for current
+                voltage.append(float(preset[0:3]))
+                current.append(float(preset[3:6]))
+            except KeyError as e:
+                raise InvalidOrNoReply from e
+
+        # Transform current in Ampere and voltage in Volt
+        current = [x / 10 for x in current]
+        voltage = [x / 10 for x in voltage]
+
+        # Usual issue with some models having higher precision for voltage
+        if self.get_info() in self.MODEL_ALT_RANGE:
+            voltage = [x / 10 for x in voltage]
+
+        return list(zip(voltage, current))
+
+    def get_preset(self, index) -> Tuple[float, float]:
+        """ Get voltage and current for given preset position [0..2] """
+        all_preset = self.get_all_preset()
+        try:
+            return all_preset[index]
+        except KeyError as e:
+            raise InvalidArgument from e
+
+    def run_preset(self, index: int) -> bool:
+        """ Set Voltage and Current using values saved in one of the three memory locations: 0, 1 or 2 """
         if not 0 <= int(index) < 3:
             raise InvalidArgument
         cmd = 'RUNM' + str(int(index))
         response = self._send_command(cmd)
-        if not response:
-            return False
-        return response
+        return response == "OK"
 
     def remove_protection(self) -> bool:
         """" I guess it removes over voltage protection """
@@ -310,7 +262,3 @@ class InstrumentInterface:
         """" I guess it adds over voltage protection """
         response = self._send_command("SPRO1")
         return bool(response)
-
-
-if __name__ == '__main__':
-    pass
