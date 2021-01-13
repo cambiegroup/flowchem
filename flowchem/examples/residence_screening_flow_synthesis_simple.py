cde81bb573c9afe5f7a41aa3382c542940159fee
Jakob Wolf <jwolf@mpikg.local>
2021-01-13 17:02:14 +0100

    working residence time screening script

--- flowchem/examples/residence_screening_flow_synthesis_simple.py
+++ flowchem/examples/residence_screening_flow_synthesis_simple.py
@@ -3,7 +3,10 @@ import logging
 from flowchem.devices.Knauer.KnauerPumpValveAPI import KnauerValve, KnauerPump
 from flowchem.devices.Knauer.knauer_autodiscover import autodiscover_knauer
 from flowchem.devices.Harvard_Apparatus.HA_elite11 import Elite11, PumpIO
+from flowchem.devices.mansonlib import PowerSupply
+import csv
 
+# usable, but maybe some output table or format wouzld be nicee
 
 
 #residence time determines flow rate with know volume
@@ -15,12 +18,16 @@ def get_flow_rate_for_residence_time(residence_time, volume_reactor):
 
 
 # this starts  the pump. loads the sample loop. pushes half the reactor volume through, then switches to next collection vial, waits half reactor + full reactor volume.
-def perform_experiment(residence_time, reactor_volume, sample_loop_volume):
+def perform_experiment(residence_time, reactor_volume, sample_loop_volume, file_name=None):
     flow_rate = get_flow_rate_for_residence_time(residence_time, reactor_volume)
 
     current_valve_position = neccessary_devices_macs['fraction_collection'].get_current_position()
 
     logging.info('Flow rate is {} for residence time {}. This will go into collection vessel on position {}'.format(flow_rate, residence_time, int(current_valve_position)+1))
+
+    if file_name:
+        with open(file_name, 'a') as f:
+            f.write('Collection vessel on valve position {2}: Flow rate was {0} ml/min for residence time {1} min.\n\r'.format(flow_rate, residence_time, int(current_valve_position)+1))
 
     neccessary_devices_macs['solvent_delivery'].set_flow(flow_rate*1000) # transorm to µl/min
     neccessary_devices_macs['solvent_delivery'].start_flow()
@@ -70,7 +77,6 @@ def perform_experiment(residence_time, reactor_volume, sample_loop_volume):
 
 
 if __name__ == "__main__":
-
     # take arguments and calculate everything needed. In the end this will solely be times and flow rates.
     # iterate through experiments
     # get available macs and check against required_macs list.  This isn't elegant for sure
@@ -83,6 +89,15 @@ if __name__ == "__main__":
         if i not in available_macs_ips:
             raise ConnectionError('At least one device with MAC {} is not available'.format(i))
 
+    # use power supply and switch leds on
+    ps = PowerSupply()
+    ps.open("COM30")
+
+    ps.set_current(2)
+    ps.output_on()
+
+    ps.set_voltage(36)
+
     neccessary_devices_macs = {'solvent_delivery': KnauerPump(available_macs_ips['00:80:a3:ba:bf:e2']),
                                'injection_loop': KnauerValve(available_macs_ips['00:80:a3:ce:7e:15']),
                                'injection_pump': Elite11(PumpIO('COM7'), diameter=30, volume_syringe=10),
@@ -91,7 +106,8 @@ if __name__ == "__main__":
     # ultimately, this could be determined with dye or fluorescence, biphasic mixture(diffusion) known flowrate would yield volume
     # reactortocollection needs to be cleaned actually. This needs to be done after everything is out, but before new stuff arrives
     reactor_volume = 4.7
-    injection_loop_volume = 3
+    # ToDo check:
+    injection_loop_volume = 1
 
 
     residence_time_list = [0.5, 1, 2, 3]#[15, 20, 25, 30, 35, 40, 45, 50]  # in min
@@ -100,7 +116,7 @@ if __name__ == "__main__":
     neccessary_devices_macs['injection_pump'].infusion_rate(5)
 
     for i in residence_time_list:
-        perform_experiment(i, reactor_volume, injection_loop_volume)
+        perform_experiment(i, reactor_volume, injection_loop_volume, file_name='/flowchem/examples/catalyst_screening_residence_time')
 
     logging.info('run finished. Now purging some solvent to get last tail out')
     neccessary_devices_macs['solvent_delivery'].set_flow(1000)
@@ -109,4 +125,4 @@ if __name__ == "__main__":
     neccessary_devices_macs['solvent_delivery'].stop_flow()
 
 
-# TODO DAMN HPLC pump always throws errors... check y
+# Future: ALL parameters should be reported. 
