ICIR is missing, wrong calculation corrected

flowchem/examples/experiments/flow_screener.py
@@ -20,22 +20,23 @@ import pandas as pd
 from time import sleep, time
 
 
-# hard parameters
+# hard parameters, in mL
 reactor_volume = 2
 
 def flow_rates(volume, residence_time, equivalents):
     total_flow = volume/residence_time
-    # 0.1 is the concentration ratio
-    flow_thio = total_flow*0.1*equivalents
-    flow_acid = total_flow-flow_thio
-    return round(flow_thio, 3), round(flow_acid, 3)
+
+    flow_acid = total_flow/(0.1*equivalents+1)
+    flow_thio =total_flow-flow_acid
+
+    return flow_thio, flow_acid
 
 
 # prepare the IO Frame
 equivalents = np.arange(start=1, stop=2.1, step=0.1)
 residence_times = np.arange(start=1, stop=11)
 
-conditions_results = pd.DataFrame(columns=['residence_time', 'eq_thio','flow_thio', 'flow_acid', 'yield_1', 'yield_2', 'yield_3', 'yield_1_rev', 'yield_2_rev', 'yield_3_rev', 'Run_forward', 'Run_backward'])
+conditions = pd.DataFrame(columns=['residence_time', 'eq_thio','flow_thio', 'flow_acid', 'yield_1', 'yield_2', 'yield_3', 'yield_1_rev', 'yield_2_rev', 'yield_3_rev', 'Run_forward', 'Run_backward'])
 
 aa = []
 bb = []
@@ -44,12 +45,15 @@ for residence_time in residence_times:
         aa.append(residence_time)
         bb.append(equivalent)
 
-conditions_results['residence_time']=aa
-conditions_results['eq_thio']=bb
+conditions['residence_time']=aa
+conditions['eq_thio']=bb
 
 # now, iterate through the dataframe and calculate flow rates
-for ind in conditions_results.index:
-    conditions_results.at[ind, 'flow_thio'], conditions_results.at[ind, 'flow_acid'] = flow_rates(reactor_volume, conditions_results.at[ind, 'residence_time'] , conditions_results.at[ind, 'eq_thio'])
+for ind in conditions.index:
+    conditions.at[ind, 'flow_thio'], conditions.at[ind, 'flow_acid'] = flow_rates(reactor_volume, conditions.at[ind, 'residence_time'] , conditions.at[ind, 'eq_thio'])
+
+# drop the plain screening file
+conditions.to_csv(f"C:/Users/jwolf/Documents/flowchem/flowchem/examples/experiments/results/{'plain_conditions_'}{round(time())}.csv")
 
 
 # Hardware
@@ -58,7 +62,12 @@ pump_connection = PumpIO('COM4')
 pump_thionyl_chloride = Elite11(pump_connection, address=0)
 pump_hexyldecanoic_acid = Elite11(pump_connection, address=1)
 
-
+# now load that one csv and output the results
+#check if a conditions results csv already exists
+try:
+    conditions_results = pd.read_csv("C:/Users/jwolf/Documents/flowchem/flowchem/examples/experiments/results/conditions_results.csv")
+except OSError:
+    conditions_results = conditions
 
 # Dataframe already is in the right order, now iterate through from top and from bottom, run the experiments and set the boolean
 # assume that the correct syringe diameter was manually set
@@ -91,13 +100,16 @@ for ind in conditions_results.index:
         # check if any pump stalled, if so, set the bool false, else true
         if pump_thionyl_chloride.is_moving() and pump_hexyldecanoic_acid.is_moving():
             conditions_results.at[ind, 'Run_forward'] = True
+            conditions_results.to_csv("C:/Users/jwolf/Documents/flowchem/flowchem/examples/experiments/results/conditions_results.csv")
+
         else:
             conditions_results.at[ind, 'Run_forward'] = False
+            conditions_results.to_csv("C:/Users/jwolf/Documents/flowchem/flowchem/examples/experiments/results/conditions_results.csv")
             break
 
 for ind in reversed(conditions_results.index):
-    if conditions_results.at[ind, 'Run_forward']  != True:
-        # also check the bool, if it ran already, don't rerun it. but skip it
+    if conditions_results.at[ind, 'Run_backward']  != True:
+        # also check the bool, if it ran already, don't rerun, but skip it
         pump_thionyl_chloride.infusion_rate = conditions_results.at[ind, 'flow_thio']
         pump_hexyldecanoic_acid.infusion_rate = conditions_results.at[ind, 'flow_acid']
         if pump_thionyl_chloride.is_moving() and pump_hexyldecanoic_acid.is_moving():
@@ -113,8 +125,9 @@ for ind in reversed(conditions_results.index):
         if pump_thionyl_chloride.is_moving() and pump_hexyldecanoic_acid.is_moving():
             pass
         else:
-            conditions_results.at[ind, 'Run_forward'] = False
+            conditions_results.at[ind, 'Run_backward'] = False
             break
+
 
         # take three IRs
         print('yield is nice')
@@ -123,16 +136,18 @@ for ind in reversed(conditions_results.index):
 
         # check if any pump stalled, if so, set the bool false, else true
         if pump_thionyl_chloride.is_moving() and pump_hexyldecanoic_acid.is_moving():
-            conditions_results.at[ind, 'Run_forward'] = True
+            conditions_results.at[ind, 'Run_backward'] = True
+            conditions_results.to_csv("C:/Users/jwolf/Documents/flowchem/flowchem/examples/experiments/results/conditions_results.csv")
+
         else:
-            conditions_results.at[ind, 'Run_forward'] = False
+            conditions_results.at[ind, 'Run_backward'] = False
+            conditions_results.to_csv("C:/Users/jwolf/Documents/flowchem/flowchem/examples/experiments/results/conditions_results.csv")
+
             break
 
-#stop the pumps
+pump_thionyl_chloride.stop()
+pump_hexyldecanoic_acid.stop()
 
-# after the whole for loop is over, drop that to a csv file.
-# TODO make sure this doesn't overwrite previous runs, by adding timestamp. Also make proper path
-conditions_results.to_csv(f"C:/Users/jwolf/Documents/flowchem/flowchem/examples/experiments/results/{'screening_at_'}{round(time())}")
 
 
 