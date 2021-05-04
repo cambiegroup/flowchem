59423baa23467e9cf3376bc922ed6b34d77a0a27
jwolf <jakob.wolf@mpikg.mpg.de>
2021-05-04 12:52:45 +0200

    tried to make yield calc work.  Yet to be tested

--- flowchem/examples/experiments/flow_screener_executer.py
+++ flowchem/examples/experiments/flow_screener_executer.py
@@ -9,15 +9,65 @@
 #  OK 2) set  flow rate for first point
 #  OK 3) start syringes once
 #  OK 4) wait for equilibration
-#  5) measure spectra and calculate yield. repeat that 3 times
+#  OK 5) measure spectra and calculate yield. repeat that 3 times
 #  C) Inputs and outputs should be:
 #  OK 1) a dictionary, also check what was used in the sugar optimizer, probably bendable. Go for pandas data frame
 #  OK kind of 2) output needs to be condition and yield. Simply append yield to empty field in data frame?
+#  MISSING: pseudovoigt & dropping the spectra to some folder, with identifier
+
 
 from flowchem.devices.Harvard_Apparatus.HA_elite11 import Elite11, PumpIO
-import numpy as np
+import opcua
+from flowchem.devices.MettlerToledo.iCIR import FlowIR
 import pandas as pd
 from time import sleep
+import scipy
+from lmfit import Minimizer, Parameters
+from lmfit.lineshapes import gaussian
+import pathlib
+
+
+def residual(pars, x, data):
+    """ Fitting model: 3 gaussian peaks centered at 1708, 1734 and 1796 cm-1 """
+    model = (gaussian(x, pars['amp_1'], 1708, pars['wid_1']) +
+             gaussian(x, pars['amp_2'], 1734, pars['wid_2']) +
+             gaussian(x, pars['amp_3'], 1796, pars['wid_3']))
+    return model - data
+
+
+# create the fitting parameters
+pfit = Parameters()
+pfit.add(name='amp_1', value=0.50, min=0)
+pfit.add(name='amp_2', value=0.50, min=0)
+pfit.add(name='amp_3', value=0.50, min=0)
+pfit.add(name='wid_1', value=2, min=4, max=12)
+pfit.add(name='wid_2', value=5, min=5, max=15)
+pfit.add(name='wid_3', value=5, min=10, max=30)
+
+
+def calculate_yield(spectrum_df):
+    spectrum_df.query(f"{1600} <= index <= {1900}", inplace=True)
+    x_arr = spectrum_df.index.to_numpy()
+    y_arr = spectrum_df[0]
+
+    mini = Minimizer(residual, pfit, fcn_args=(x_arr, y_arr))
+    out = mini.leastsq()
+    # Get fitted gaussians
+    g1 = gaussian(x_arr, out.params["amp_1"], 1708, out.params["wid_1"])
+    g2 = gaussian(x_arr, out.params["amp_2"], 1734, out.params["wid_2"])
+    g3 = gaussian(x_arr, out.params["amp_3"], 1796, out.params["wid_3"])
+
+    # Calculate yield based on fitted peaks (less sensitive to baseline drift)
+    acid = scipy.integrate.trapezoid(g1 + g2, x_arr)
+    chloride = scipy.integrate.trapezoid(g3, x_arr)
+    latest_yield = chloride / (chloride + acid)
+    print(f"yield is {latest_yield}")
+    return latest_yield
+
+
+root = pathlib.Path()
+path_to_write_csv = root.resolve().joinpath('tests')
+
 
 # Hardware
 pump_connection = PumpIO('COM5')
@@ -28,10 +78,28 @@ pump_hexyldecanoic_acid = Elite11(pump_connection, address=6)
 pump_thionyl_chloride.syringe_diameter = 9.62
 pump_hexyldecanoic_acid.syringe_diameter = 19.93
 
+
+###
+# initialise the IR and make sure everything works
+client = opcua.Client(url=FlowIR.iC_OPCUA_DEFAULT_SERVER_ADDRESS)
+ir_spectrometer = FlowIR(client)
+if ir_spectrometer.is_iCIR_connected:
+    print(f"FlowIR connected!")
+else:
+    print("FlowIR not connected :(")
+    import sys
+    sys.exit()
+
+spectrum = ir_spectrometer.get_last_spectrum_treated()
+while spectrum.empty:
+    spectrum = ir_spectrometer.get_last_spectrum_treated()
+
+###
+
 try:
-    conditions_results = pd.read_csv("flow_screening_experiment.csv")
+    conditions_results = pd.read_csv(path_to_write_csv.joinpath("flow_screening_experiment.csv"))
 except OSError:
-    conditions_results = pd.read_csv("flow_screening_empty.csv")
+    conditions_results = pd.read_csv(path_to_write_csv.joinpath("flow_screening_empty.csv"))
 
 # Dataframe already is in the right order, now iterate through from top and from bottom, run the experiments and set the boolean
 # assume that the correct syringe diameter was manually set
@@ -56,22 +124,30 @@ for ind in conditions_results.index:
         # check if any pump stalled, if so, set the bool false, leave loop
         if not pump_thionyl_chloride.is_moving() or not pump_hexyldecanoic_acid.is_moving():
             conditions_results.at[ind, 'Run_forward'] = False
-            conditions_results.to_csv("flow_screening_experiment.csv")
+            conditions_results.to_csv(path_to_write_csv.joinpath("flow_screening_experiment.csv"))
             break
 
-        # take three IRs
-        for i in range(3):
-            pass
+        # do this 3 times, just gets 3 consecutive spectra
+        for x in range(3):
+            spectra_count = ir_spectrometer.get_sample_count()
+
+            while ir_spectrometer.get_sample_count() == spectra_count:
+                sleep(1)
+
+            print(f"New spectrum!")
+            spectrum = ir_spectrometer.get_last_spectrum_treated()
+            spectrum_df = spectrum.as_df()
+            conditions_results.at[ind, f'yield_{x+1}'] = calculate_yield(spectrum_df)
 
 
         # check if any pump stalled, if so, set the bool false, else true
         if not pump_thionyl_chloride.is_moving() or not pump_hexyldecanoic_acid.is_moving():
             conditions_results.at[ind, 'Run_forward'] = False
-            conditions_results.to_csv("flow_screening_experiment.csv")
+            conditions_results.to_csv(path_to_write_csv.joinpath("flow_screening_experiment.csv"))
             break
         else:
             conditions_results.at[ind, 'Run_forward'] = True
-            conditions_results.to_csv("flow_screening_experiment.csv")
+            conditions_results.to_csv(path_to_write_csv.joinpath("flow_screening_experiment.csv"))
 
 
 for ind in reversed(conditions_results.index):
@@ -91,22 +167,30 @@ for ind in reversed(conditions_results.index):
         # check if any pump stalled, if so, set the bool false, leave loop
         if not pump_thionyl_chloride.is_moving() or not pump_hexyldecanoic_acid.is_moving():
             conditions_results.at[ind, 'Run_backward'] = False
-            conditions_results.to_csv("flow_screening_experiment.csv")
+            conditions_results.to_csv(path_to_write_csv.joinpath("flow_screening_experiment.csv"))
             break
 
-        # take three IRs
-        print('yield is nice')
-        # easiest: no triggering but extraction from working live data visualisation
+        # do this 3 times, just gets 3 consecutive spectra
+        for x in range(3):
+            spectra_count = ir_spectrometer.get_sample_count()
 
+            while ir_spectrometer.get_sample_count() == spectra_count:
+                sleep(1)
+
+            print(f"New spectrum!")
+            spectrum = ir_spectrometer.get_last_spectrum_treated()
+            spectrum_df = spectrum.as_df()
+            # this now needs to be translated to yield
+            conditions_results.at[ind, f'yield_{x+1}_rev'] = calculate_yield(spectrum_df)
 
         # check if any pump stalled, if so, set the bool false, else true
         if pump_thionyl_chloride.is_moving() and pump_hexyldecanoic_acid.is_moving():
             conditions_results.at[ind, 'Run_backward'] = True
-            conditions_results.to_csv("flow_screening_experiment.csv")
+            conditions_results.to_csv(path_to_write_csv.joinpath("flow_screening_experiment.csv"))
 
         else:
             conditions_results.at[ind, 'Run_backward'] = False
-            conditions_results.to_csv("flow_screening_experiment.csv")
+            conditions_results.to_csv(path_to_write_csv.joinpath("flow_screening_experiment.csv"))
 
             break
 
--- flowchem/examples/experiments/flow_screener_planner.py
+++ flowchem/examples/experiments/flow_screener_planner.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
+import pathlib
 
 # hard parameters
 reactor_volume = 0.05
-
 
 def flow_rates(volume, residence_time, equivalents):
     """
@@ -21,6 +21,15 @@ def flow_rates(volume, residence_time, equivalents):
     return flow_thio, flow_acid
 
 
+def calculate_flowrate(row):
+    """ Function to be applied to the dataframe to populate flowrates """
+    row['flow_thio'], row['flow_acid'] = flow_rates(reactor_volume, row['residence_time'], row['eq_thio'])
+    return row
+
+
+root = pathlib.Path()
+path_to_write_csv = root.resolve().joinpath('tests')
+
 # prepare the IO Frame
 equivalents = np.linspace(start=1, stop=2, num=11)  # [1.  1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2. ]
 residence_times = np.linspace(start=1, stop=10, num=10)  # [ 1.  2.  3.  4.  5.  6.  7.  8.  9. 10.]
@@ -29,18 +38,19 @@ conditions_results = pd.DataFrame(columns=['residence_time', 'eq_thio', 'flow_th
                                            'yield_3', 'yield_1_rev', 'yield_2_rev', 'yield_3_rev', 'Run_forward',
                                            'Run_backward'])
 
+conditions_results.Run_forward = conditions_results.Run_forward.astype(bool)
+conditions_results.Run_backward = conditions_results.Run_backward.astype(bool)
+
+
 # Create tuples with (residence time, equivalents) for each experimental datapoint
 experimental_conditions = [(t_res, eq) for t_res in residence_times for eq in equivalents]
 # Adds conditions to dataframe
 conditions_results['residence_time'], conditions_results['eq_thio'] = zip(*experimental_conditions)
 
 
-def calculate_flowrate(row):
-    """ Function to be applied to the dataframe to populate flowrates """
-    row['flow_thio'], row['flow_acid'] = flow_rates(reactor_volume, row['residence_time'], row['eq_thio'])
-    return row
+
 
 
 # now, iterate through the dataframe and calculate the needed flow rates
 conditions_results: pd.DataFrame = conditions_results.apply(calculate_flowrate, axis=1)
-conditions_results.to_csv("flow_screening_empty.csv")
+conditions_results.to_csv(path_to_write_csv.joinpath("flow_screening_empty.csv"))
