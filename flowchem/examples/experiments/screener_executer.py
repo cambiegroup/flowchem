bbd9f62d746b7c49a942eca9c4bdbbb044e9b34f
Dario Cambie <dario.cambie@mpikg.mpg.de>
2021-05-14 12:08:14 +0200

    Improve screener

--- flowchem/examples/experiments/screener_executer.py
+++ flowchem/examples/experiments/screener_executer.py
@@ -62,9 +62,10 @@ def remove_xp_from_source_df(index_to_remove):
     source_df.drop(index_to_remove, inplace=True)
     source_df.to_csv(SOURCE_FILE)
 
+
 # Loop execute the points that are needed
 for index, row in xp_data.iterrows():
-    print(row)
+    print(f"Applying the follwoiung conditions: tR={row['tR']}, SOCl2_eq={row['eq']}, temp={row['T']}")
 
     # Once experiment is performed remove it from the source CSV
     #remove_xp_from_source_df(index_to_remove=index)
