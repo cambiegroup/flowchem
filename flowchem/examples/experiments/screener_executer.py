28d26400dd5fb6b71748627422f1ed8e97de0429
Dario Cambie <dario.cambie@mpikg.mpg.de>
2021-05-16 19:56:10 +0200

    Minor changes to executer

--- flowchem/examples/experiments/screener_executer.py
+++ flowchem/examples/experiments/screener_executer.py
@@ -19,9 +19,10 @@ from flowchem.devices.MettlerToledo.iCIR import FlowIR
 from flowchem.devices.Vapourtec.R4_heater import R4Heater, VapourtecCommand
 from flowchem.examples.experiments.step_one_chemometrics import measure_yield_step1
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+logging.getLogger('opcua').setLevel(logging.CRITICAL)
 
 # FILES AND PATHS
 WORKING_DIR = Path().home() / "Documents"
@@ -55,17 +56,20 @@ pump_socl2_filling = Elite11(elite_pump_connection, address=1, diameter=14.6)  #
 _loopA = 0.4
 
 # Thionyl chloride - pumping
-ml600_socl2_connection = HamiltonPumpIO(port="COM43", hw_initialization=False)
-pump_socl2_solvent = ML600(ml600_socl2_connection, syringe_volume=5)
-# Default initialization speed too high and defaults to outlet, reactor side, high pressure drop so it fails...
-# To save solvent and be faster we can assume the pump has already been primed (there´s a button just for that)
-# And initialize the valve, switching it to INPUT before syringe initialization and recycle the solvent
-pump_socl2_solvent.stop()
-pump_socl2_solvent.return_steps = 100  # This is important to ensure the two loops meet at the same time at the Tee
-pump_socl2_solvent.initialize_valve()
-pump_socl2_solvent.valve_position = pump_socl2_solvent.ValvePositionName.INPUT
-pump_socl2_solvent.initialize_syringe(speed=30)
-pump_socl2_solvent.log.setLevel(logging.DEBUG)
+_pump_socl2_mac = '00:80:a3:ba:bf:e2'
+available_knauer_devices = autodiscover_knauer(source_ip='192.168.1.1')
+pump_socl2_solvent = KnauerPump(available_knauer_devices[_pump_socl2_mac])
+
+# Injection valve A
+_valve_A_mac = '00:80:a3:ce:7e:c4'
+valveA = KnauerValve(available_knauer_devices[_valve_A_mac])
+valveA.switch_to_position("INJECT")  # Not necessary, used to check communication
+
+# Injection valve B
+_valve_B_mac = '00:80:a3:ce:7e:15'
+valveB = KnauerValve(available_knauer_devices[_valve_B_mac])
+valveB.switch_to_position("INJECT")  # This is better on inj before ML600 initialization or prevent loop contamination
+
 
 # loop B - 5.0 ml - filling
 # Hexyldecanoic acid - filling
@@ -76,31 +80,19 @@ _loopB = 4
 
 # Hexyldecanoic acid - pumping
 _pump_acid_mac = '00:20:4a:cd:b7:44'
-available_knauer_devices = autodiscover_knauer(source_ip='192.168.1.1')
-try:
-    pump_acid_solvent = KnauerPump(available_knauer_devices[_pump_acid_mac])
-except KeyError as e:
-    raise RuntimeError("Acid pump unreachable. Is it connected and powered on?") from e
+pump_acid_solvent = KnauerPump(available_knauer_devices[_pump_acid_mac])
 
-# Injection valve A
-_valve_A_mac = '00:80:a3:ce:7e:c4'
-valveA = KnauerValve(available_knauer_devices[_valve_A_mac])
-valveA.switch_to_position("LOAD")  # Not necessary, used to check communication
-
-# Injection valve B
-_valve_B_mac = '00:80:a3:ce:7e:15'
-valveB = KnauerValve(available_knauer_devices[_valve_B_mac])
-valveB.switch_to_position("LOAD")  # Not necessary, used to check communication
 
 # Stop loop-filling pumps and start infusion pumps
 
 # Ensure pump initializations are over
 pump_acid_filling.wait_until_idle()
-pump_socl2_solvent.wait_until_idle()
 
 # Start HPLC pump
 pump_acid_solvent.set_flow(0.1)
 pump_acid_solvent.start_flow()
+pump_socl2_solvent.set_flow(0.1)
+pump_socl2_solvent.start_flow()
 
 
 def calculate_flowrate(residence_time: float, equivalent: float) -> Tuple[float, float]:
@@ -133,7 +125,8 @@ def xp_measure_yield(max_scan: int) -> Tuple[float, float]:
 
         # Evaluate if results are stable enough to return
         if len(measured_yield) >= 3:
-            n_yield = np.array(measured_yield)
+            # Consider only the last 3 points
+            n_yield = np.array(measured_yield[-3:])
             if n_yield.var() < 0.01 or len(measured_yield) > max_scan:
                 print(f"Stop analytics! Calculated yield is {n_yield.mean()} CV {n_yield.var()} after "
                       f"{len(measured_yield)} measurements!")
@@ -169,26 +162,18 @@ for index, row in xp_data.iterrows():
     logger.debug(f"Setting temperature to {row['T']}")
 
     # 2) Stops and reload ML600 to target
-    pump_socl2_solvent.stop()
-    pump_socl2_solvent.valve_position = pump_socl2_solvent.ValvePositionName.INPUT
-    pump_socl2_solvent.to_volume(5.0, speed=30)  # Refill at 10 ml/min
-
     pump_acid_filling.stop()
     pump_acid_filling.valve_position = pump_acid_filling.ValvePositionName.INPUT
     pump_acid_filling.to_volume(_loopB, speed=30)  # Refill at 10 ml/min
-
-    # Wait for target reached
-    pump_socl2_solvent.wait_until_idle()
-    pump_acid_filling.wait_until_idle()
+    pump_acid_filling.wait_until_idle()  # Wait for target reached
 
     # Switch to outlet
-    pump_socl2_solvent.valve_position = pump_socl2_solvent.ValvePositionName.OUTPUT
     pump_acid_filling.valve_position = pump_acid_filling.ValvePositionName.OUTPUT
     logger.debug(f"Hamilton s.pump reloaded")
 
     # 3) Set flowrates of solvent pumps
     _flowrate_socl2, _flowrate_acid = calculate_flowrate(row["tR"], row["eq"])
-    pump_socl2_solvent.to_volume(0, speed=pump_socl2_solvent.flowrate_to_seconds_per_stroke(_flowrate_socl2))
+    pump_socl2_solvent.set_flow(_flowrate_socl2)
     pump_acid_solvent.set_flow(_flowrate_acid)
     logger.debug(f"Started solvent infusion at target flow rate (SOCl2={_flowrate_socl2}, acid={_flowrate_acid})")
 
@@ -213,14 +198,14 @@ for index, row in xp_data.iterrows():
 
     # 7) Switch valves to inject
     sleep(3)  # Ensure filling is complete
-    valveA.switch_to_position("INJECT")
     valveB.switch_to_position("INJECT")
+    valveA.switch_to_position("INJECT")
     logger.info(f"Experiment started!")
     start_time = time()
 
-    # 8) Waits 1.5 tR, and measure yield and save
-    sleep(60 * row["tR"] * 1.5)
-    logger.debug(f"Reached 1.5 tR")
+    # 8) Waits 2 tR, and measure yield and save
+    sleep(60 * row["tR"] * 2)
+    logger.debug(f"Reached 2 tR")
 
     # Max 1.5 tR time to measure yield, integration is 15" so 4 scans per minute
     max_scan = row["tR"] * 4 * 1.5
@@ -237,21 +222,24 @@ for index, row in xp_data.iterrows():
     logger.debug(f"Updated results in {OUTPUT_FILE}")
 
     # 9) flushes loops+reactor at higher flowrate
-    # For the flushes reach 6 reactor volumes.
+    # For the flushes reach 5 reactor volumes.
     current_time = time()
     reactor_volumes_infused = (current_time - start_time) / (row["tR"] * 60)
-    reactor_volumes_to_flush = 6 - reactor_volumes_infused
+    reactor_volumes_to_flush = 5 - reactor_volumes_infused
     # Now let's flush the remaining reactor volume with the flowrate of 1min tR
     _fast_flowrate_socl2, _fast_flowrate_acid = calculate_flowrate(1, row["eq"])  # Flush flowrate is tR=1
-    pump_socl2_solvent.to_volume(0, speed=pump_socl2_solvent.flowrate_to_seconds_per_stroke(_flowrate_socl2))
+    pump_socl2_solvent.set_flow(_flowrate_socl2)
     pump_acid_solvent.set_flow(_fast_flowrate_acid)
+    logger.debug(f"Waiting {reactor_volumes_to_flush} reactor volume for the reaction to leave the reactor.")
     sleep(reactor_volumes_to_flush*60)
 
     # Once experiment is performed remove it from the source CSV
-    # source_df.drop(index, inplace=True)
-    # source_df.to_csv(SOURCE_FILE)
+    xp_data.drop(index, inplace=True)
+    xp_data.to_csv(SOURCE_FILE)
     logger.info(f"XP completed!")
 
-
+# Stop all
 pump_acid_filling.stop()
 pump_socl2_filling.stop()
+pump_socl2_solvent.stop_flow()
+pump_acid_solvent.stop_flow()
