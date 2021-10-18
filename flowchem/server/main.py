28620ce7341183942050dcfec12d44196dcc6106
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-10-18 13:33:05 +0200

    Minor changes, still on #14 integration

--- flowchem/server/main.py
+++ flowchem/server/main.py
@@ -7,20 +7,18 @@ import flowchem
 import inspect
 import itertools
 from types import ModuleType
-from typing import Iterable, Dict
-
+from typing import Iterable, Dict, Tuple, Union
 
 from fastapi import FastAPI
 from mdns_server import Server_mDNS
 from device_node_creator import DeviceNode
 from microflow import devices
 
-logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 # packages containing the device class definitions. Target classes should be available in the module top level.
 DEVICE_MODULES = [flowchem, devices]
+
 
 def get_device_class_mapper(modules: Iterable[ModuleType]) -> Dict[str, type]:
     """
@@ -41,20 +39,28 @@ def get_device_class_mapper(modules: Iterable[ModuleType]) -> Dict[str, type]:
     return {k: v for (k, v) in itertools.chain.from_iterable(objects_in_modules)}
 
 
-def create_server_from_config(config_file: Path) -> FastAPI:
-    # Get config
-    with config_file.open() as stream:
-        config = yaml.safe_load(stream)
+def create_server_from_config(config: Union[Path, Dict]) -> Tuple[FastAPI, Server_mDNS]:
+    """
+
+    Args:
+        config: Path to the yaml file with the device config or dict.
+
+    Returns:
+
+    """
+    if isinstance(config, Path):
+        with config.open() as stream:
+            config = yaml.safe_load(stream)
 
     # FastAPI server
-    app = FastAPI()
+    app = FastAPI(title="flowchem", version=flowchem.__version__)
 
     # Zeroconf server
     zeroconf = Server_mDNS()
 
     # Device mapper
     device_mapper = get_device_class_mapper(DEVICE_MODULES)
-    logger.debug(f"The followinf device classes have been found: {device_mapper.keys()}")
+    logger.debug(f"The following device classes have been found: {device_mapper.keys()}")
 
     # Parse list of devices and generate endpoints
     for device_name, device_config in config.items():
@@ -76,6 +82,9 @@ def create_server_from_config(config_file: Path) -> FastAPI:
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+
     app, zeroconf = create_server_from_config(Path("sample_config.yml"))
 
     @app.get("/")
--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+def get_version(rel_path: str) -> str:
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            # __version__ = "0.9"
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
 setuptools.setup(
     name="flowchem",
-    version="0.0.3dev",
+    version=get_version("flowchem/__init__.py"),
     author="Dario Cambié, Jakob Wolf",
     author_email="dario.cambie@mpikg.mpg.de, jakob.wolf@mpikg.mpg.de",
     description="Flowchem is a python library to control a variety of instruments commonly found in chemistry labs.",
