b4cdd9c89e8799c9a81aba5830e151133bfb79ee
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-10-19 10:06:06 +0200

    Huber stub

--- flowchem/devices/Huber/huber.py
+++ flowchem/devices/Huber/huber.py
@@ -1,10 +1,33 @@
+"""
+Driver for Huber chillers.
+"""
+import logging
+import aioserial
+import asyncio
 
 
 class Huber:
     """
     Control class for Huber chillers.
     """
-    def __init__(self):
-        pass
+    def __init__(self, aio: aioserial.AioSerial):
+        self._serial = aio
 
-    #
+    async def send_command_and_read_reply(self, command: str) -> str:
+        await self._serial.write_async(command.encode("ascii"))
+        reply = await self._serial.readline_async()
+        return reply.decode("ascii")
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    async def main(chiller: Huber):
+        rep = await chiller.send_command_and_read_reply("test")
+        return rep
+
+
+    chiller = Huber(aioserial.AioSerial(port='COM3', baudrate=9600))
+    coro = main(chiller)
+    asyncio.run(coro)
