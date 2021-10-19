e7e740fd6ea8d3f098cec2847ded82a4afd26065
Dario Cambié <dario.cambie@glasgow.ac.uk>
2021-10-19 17:04:15 +0200

    Add test for HuberChiller.status()
    And scaffolding for other methods

--- flowchem/devices/Huber/huberchiller.py
+++ flowchem/devices/Huber/huberchiller.py
@@ -29,6 +29,9 @@ class ChillerStatus:
     def __repr__(self):
         return str(self.__dict__)
 
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
 
 @dataclass
 class PBCommand:
--- tests/test_huber.py
+++ tests/test_huber.py
@@ -1,1 +1,34 @@
-# TODO
+import aioserial
+import pytest
+from flowchem.devices.Huber.huberchiller import HuberChiller, ChillerStatus
+
+
+class FakeSerial(aioserial.AioSerial):
+    """ Mock AioSerial. """
+    def __init__(self):
+        self.fixed_reply = None
+        self.last_command = b""
+        self.map_reply = {
+            b"{M0A****\r\n": b"{S0AFFFF",
+        }
+
+    async def write_async(self, text: bytes):
+        self.last_command = text
+
+    async def readline_async(self,  size: int = -1) -> bytes:
+        if self.fixed_reply:
+            return self.fixed_reply
+        return self.map_reply[self.last_command]
+
+
+@pytest.fixture(scope="session")
+def chiller():
+    """ Chiller instance connected to FakeSerial """
+    return HuberChiller(FakeSerial())
+
+
+@pytest.mark.asyncio
+async def test_status(chiller):
+    stat = await chiller.status()
+    print(stat)
+    assert stat == ChillerStatus("1111111111111111")
