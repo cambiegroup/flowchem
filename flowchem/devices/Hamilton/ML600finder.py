41aec812a82bce302d671007a2eadf0371635c39
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-10-20 17:45:10 +0200

    improve doc strings

--- flowchem/devices/Hamilton/ML600finder.py
+++ flowchem/devices/Hamilton/ML600finder.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 
 
 def ml600_finder():
-    # List of all serial port available
+    """ Try to initialize an ML600 on every available COM port. """
     port_available = [comport.device for comport in serial.tools.list_ports.comports()]
 
     # Ports connected to an ML600-looking device
@@ -31,4 +31,4 @@ if __name__ == "__main__":
     if len(ml600_pumps) > 0:
         print(f"The following serial port are connected to ML600: {ml600_pumps}")
     else:
-        print("No ML600 found")
+        print("No ML600 pump found")
