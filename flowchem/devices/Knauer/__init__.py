b6ff34969cf900eaa39a0fcf8a7890ee0c580d48
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-10-28 22:58:21 +0200

    Add pumps and valve to top level

--- flowchem/devices/Knauer/__init__.py
+++ flowchem/devices/Knauer/__init__.py
@@ -1,4 +1,4 @@
 from .AzuraCompactPump import AzuraCompactPump
-from .KnauerValve import KnauerValve
-from .Knauer_common import autodiscover_knauer
+from .KnauerValve import Knauer12PortValve, Knauer16PortValve, Knauer6Port6PositionValve, Knauer6Port2PositionValve
+from .Knauer_autodiscover import autodiscover_knauer
 
