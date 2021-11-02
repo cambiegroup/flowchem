529771f2d1aede353908f304e22bfbb3751af572
jwolf <jakob.wolf@mpikg.mpg.de>
2021-11-02 09:53:12 +0100

    trying to start hplc interface from configuration, this is only started work and not yet functional

--- flowchem/devices/Knauer/__init__.py
+++ flowchem/devices/Knauer/__init__.py
@@ -1,4 +1,5 @@
 from .AzuraCompactPump import AzuraCompactPump
 from .KnauerValve import Knauer12PortValve, Knauer16PortValve, Knauer6Port6PositionValve, Knauer6Port2PositionValve
 from .Knauer_autodiscover import autodiscover_knauer
+from .HPLC_control import ClarityInterface
 
