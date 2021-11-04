686cb9557d62742e8a85a98b854ca5e2a2b3b4e6
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-11-04 14:18:30 +0100

    Working basic implementation

--- flowchem/__init__.py
+++ flowchem/__init__.py
@@ -1,3 +1,1 @@
-from .devices import *
-
 __version__ = "0.0.3"
---
+++ flowchem/mechwolf_components/FlowchemVessel.py
@@ -0,0 +1,30 @@
+from typing import *
+
+import mechwolf as mw
+
+
+class FlowchemVessel(mw.Vessel):
+    """
+    A mw.Vessel with additional properties for chemical identity
+
+    """
+
+    metadata = {
+        "author": [
+            {
+                "first_name": "Dario",
+                "last_name": "Cambie",
+                "email": "dario.cambie@mpikg.mpg.de",
+                "institution": "Max Planck Institute of Colloids and Interfaces",
+                "github_username": "dcambie",
+            }
+        ],
+        "stability": "beta",
+        "supported": True,
+    }
+
+    def __init__(self,
+                 description: Optional[str] = None,
+                 name: Optional[str] = None):
+        super().__init__(name=name, description=description)
+        # TODO: add more properties according to ORD
