8ceada8f9f3dd1092816d9ead429c80f740f87e6
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-11-04 11:18:31 +0100

    Add wrapper around AzuraCompactPump to make it mechwolf compatible
    Base class Pump requires a rate attribute as pint.Quantity and that's it.

---
+++ flowchem/mechwolf_components/KnauerAzuraPump.py
@@ -0,0 +1,58 @@
+from mechwolf import Pump
+from mechwolf import _ureg
+
+
+class KnauerAzuraPump(Pump):
+    """
+    A Knauer Azura Compact P2.1 pump
+
+    Please see : https://www.knauer.net/Dokumente/pumps/azura/manuals/V6870%20_P2.1S_P4.1S_Instructions.pdf
+
+
+    - `mac_address`: Mac address of the pump
+
+    """
+
+    metadata = {
+        "author": [
+            {
+                "first_name": "Dario",
+                "last_name": "Cambie",
+                "email": "dario.cambie@mpikg.mpg.de",
+                "institution": "Max Planck Institute of Colloids and interfaces",
+                "github_username": "dcambie",
+            }
+        ],
+        "stability": "beta",
+        "supported": True,
+    }
+
+    def __init__(self, mac_address, name=None):
+        super().__init__(name=name)
+        self.rate = _ureg.parse_expression("0 ml/min")
+        self.mac_address = mac_address
+
+    def __enter__(self):
+        from flowchem import AzuraCompactPump
+        self._pump = AzuraCompactPump.from_config(mac_address=self.mac_address)
+        await self._pump.start_flow()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._pump.stop_flow()
+        del self._pump
+
+    async def _set_flow(self, flow_rate):
+        """
+        Sets the target flow rate
+
+        Args:
+            flow_rate: a pint Quantity
+
+        Returns: None
+
+        """
+        self._pump.set_flow(setpoint_in_ml_min=flow_rate.to(_ureg.ml / _ureg.min).magnitude)
+
+    async def _update(self):
+        await self._set_flow(self.rate)
