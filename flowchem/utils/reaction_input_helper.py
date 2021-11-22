52948743d87260a299eb8c946c3151372bb5439d
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-11-22 10:34:54 +0100

    Add helper method to add flowrate to reaction input

---
+++ flowchem/utils/reaction_input_helper.py
@@ -0,0 +1,23 @@
+from ord_schema.proto.reaction_pb2 import ReactionInput, Compound, CrudeComponent, ReactionRole, ReactionIdentifier, CompoundPreparation, FlowRate
+from flowchem.units import flowchem_ureg
+
+
+def add_flowrate_to_input(reaction: ReactionInput, flowrate: str):
+    """ Add a flowrate to a reaction. """
+
+    # Parse the flowrate
+    flowrate = flowchem_ureg(flowrate)
+    assert flowrate.units == flowchem_ureg.volume / flowchem_ureg.time, "Flowrate must be in units of volume/time"
+
+    # Convert it to ml/min (we could use different ORD values, but this is easier)
+    flowrate.ito(flowchem_ureg.milliliter / flowchem_ureg.minute)
+
+    # Make it into a protobuf
+    flow_rate_pb = FlowRate()
+    flow_rate_pb.value = flowrate.magnitude
+    flow_rate_pb.units = FlowRate.FlowRateUnit.Value("MILLILITER_PER_MINUTE")
+
+    # Add the flowrate to the reaction
+    reaction.flow_rate.CopyFrom(flow_rate_pb)
+
+    return reaction
