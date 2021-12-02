916b18970909839b63310f79de671fc3d3a1b8b9
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-12-02 18:57:38 +0100

    Add coverage to nox

--- noxfile.py
+++ noxfile.py
@@ -2,18 +2,19 @@ import nox
 
 
 @nox.session(python=['3.9', '3.10'])
-def tests(session):
-    session.install('pytest', 'pytest-asyncio', 'pytest-cov')
-    session.install('.')
-    session.run('pytest')
-
-
-@nox.session
 def lint(session):
     session.install('flake8')
     session.run('flake8', 'flowchem', '--count', '--select=E9,F63,F7,F82', '--show-source', '--statistics')
 
-@nox.session
+
+@nox.session(python=['3.9', '3.10'])
 def type_check(session):
     session.install('mypy')
     session.run('mypy', '--install-types', '--non-interactive', '--python-version 3.9', 'flowchem')
+
+
+@nox.session(python=['3.9', '3.10'])
+def tests(session):
+    session.install('pytest', 'pytest-asyncio', 'pytest-cov')
+    session.install('.')
+    session.run('pytest', '--cov flowchem', '--cov-fail-under=40')
