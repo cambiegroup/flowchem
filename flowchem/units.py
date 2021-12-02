c89232d5e11a6c79b6c1b28d39c4ec0b6041abbc
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-12-02 18:25:36 +0100

    Don't use external file for units definition
    due to packaging issue.

--- flowchem/units.py
+++ flowchem/units.py
@@ -4,6 +4,5 @@ import os
 import pint
 
 flowchem_ureg = pint.UnitRegistry(autoconvert_offset_to_baseunit=True)
-
-UNIT_REGISTER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "units.txt")
-flowchem_ureg.load_definitions(UNIT_REGISTER)
+flowchem_ureg.define("step = []")
+flowchem_ureg.define("stroke = 48000 * step")
