612461e5fb2a6e1bf17f899192f13b89544c91e5
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-12-02 19:49:00 +0100

    Add "ipywidgets" to setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ setuptools.setup(
         "graphviz",
         "ord-schema",
         "google-api-python-client",
-        "rich"
+        "rich",
+        "ipywidgets"
     ],
     python_requires=">=3.8",
     entry_points={"console_scripts": ["flowchem=flowchem.cli:main"]},
