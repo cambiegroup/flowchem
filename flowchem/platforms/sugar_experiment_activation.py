c2e00c7719c7f504d15d1d1cef49636fab214399
bs-flowlab <jakob.wolf@mpikg.mpg.de>
2021-12-08 14:42:55 +0100

    cleaned imports and put the experimentsconditions, error and data_saving_retrieving to separate files

--- flowchem/platforms/sugar_experiment_activation.py
+++ flowchem/platforms/sugar_experiment_activation.py
@@ -1,4 +1,4 @@
-from time import sleep, asctime
+from time import sleep
 from flowchem.devices.Petite_Fleur_chiller import Huber
 import queue
 from threading import Thread
@@ -10,69 +10,11 @@ from flowchem.devices.Knauer.KnauerPumpValveAPI import KnauerPump
 from flowchem.miscellaneous_helpers.folder_listener import FileReceiver, ResultListener
 from flowchem.devices.ViciValco.ViciValco_Actuator import ViciValco
 import logging
-from typing import Union
 from flowchem.constants import flowchem_ureg
 from numpy import array, sum
-from pandas import read_csv, errors
 from pathlib import Path
-import json
-import dataclasses
-
-class UnderDefinedError(AttributeError):
-    pass
-
-
-
-
-class SaveRetrieveData:
-    """
-    Class that can handle saving and loading experiment data
-    It creates a new folder of experiment name, and populates that with experiment data files
-    this files can be loaded again to objects, single or in batch
-    """
-    class EnhancedJSONEncoder(json.JSONEncoder):
-        def default(self, o):
-            if dataclasses.is_dataclass(o):
-                return dataclasses.asdict(o)
-            return super().default(o)
-
-    def __init__(self, experiment_folder_name, experiment_folder_path= r"defaultpathblabla", file_extension=".soe"):
-
-        self.experiment_folder = Path(experiment_folder_path, experiment_folder_name)
-        self.file_extension = file_extension
-
-    def make_experiment_folder(self):
-        try:
-            Path.mkdir(self.experiment_folder, parents=True, exist_ok=False)
-        except FileExistsError:
-            # load already performed experiments
-            pass
-            # instead check if any of the queue elements is already present as measured examples
-
-    def save_data(self, single_experiment_file_name, single_experiment):
-        # save a new piece of data to the folder
-        with open(Path(self.experiment_folder, single_experiment_file_name+self.file_extension), 'w') as new_experiment_data_file:
-            json.dump(single_experiment, new_experiment_data_file, cls=self.EnhancedJSONEncoder)
-
-#td do same magic here/folderpath should always be appended
-    def load_and_decode_single(self, experiment_data_file_name: Union[str, Path]):
-        with open(Path(self.experiment_folder, experiment_data_file_name), 'r') as f:
-            loaded: dict = json.load(f)
-        # remove the expcode as a key - actually, already saving should be done with T in filename, maybe expname_T_expcode
-        #unpack the dictionary and hand to class for reconstruction
-        return ExperimentConditions(**loaded)
-
-
-    def load_and_decode_batch(self):
-        loaded_experiments = {}
-        for files in self.experiment_folder.glob("*" + self.file_extension):
-            one_condition = self.load_and_decode_single(files)
-            if not loaded_experiments[one_condition.temperature]:
-                loaded_experiments[one_condition.temperature] = one_condition
-            else:
-                logging.warning("Could not be loaded since same temperature was already loaded - probably another "
-                                "parameter is different. Yet to be implemented")
-        return loaded_experiments
+from flowchem.platforms.experiment_conditions import ExperimentConditions
+from flowchem.platforms.Saving_Retrieving import SaveRetrieveData
 
 #ToDo:
 # optional 1b) Check the experiments in the folder in the beginning, if experiments already performed -> take from queue and don't execute
@@ -88,102 +30,7 @@ class SaveRetrieveData:
 # TODO combining the sample name with the commit hash would make the experiment even more traceable. probably a
 #  good idea...
 # what I don't really get is why the class attributes are not printed to dict
-@dataclasses.dataclass
-class ExperimentConditions:
-    """This is actively changed, either by human or by optimizer. Goes into the queue.
-    When the conditions are taken from the queue, a FlowConditions object is derived from ExperimentCondition.
-    ExperimentConditions has to be dropped into a database, eventually, with analytic results and the optimizer
-    activated"""
 
-    # fixed, only changed for a new experiment sequence (if stock solution changes)
-    _stock_concentration_donor = "0.09 molar"
-    _stock_concentration_activator = "0.156 molar"
-    _stock_concentration_quencher = "0.62 molar"
-    _analysis_time = '16 min'
-    # how many reactor volumes until steady state reached
-    _reactor_volumes: float = 2.5
-    _quencher_eq_to_activator = 50
-
-    # alternative: hardcode flowrates
-
-    residence_time: str = "300 s"
-    concentration_donor: str = _stock_concentration_donor
-    activator_equivalents: float = 1.7333
-    _quencher_equivalents = _quencher_eq_to_activator * activator_equivalents
-    temperature: str = "25  °C"
-
-    building_block_smiles = None
-
-    _experiment_finished: bool = False
-
-    _experiment_failed: bool = False
-    _experiment_id: int = None
-
-    # when fully analysed, hold a dataframe of the chromatogram. Once automatic analysis/assignment works,
-    # this should also go in
-    _chromatogram: dict = None
-
-    def check_experiment_conditions_completeness(self):
-        if not self.building_block_smiles:
-            raise UnderDefinedError('Please provide SMILES for your BB. Draw your BB in Chemdraw, select, press Alt+Ctrl+C'
-                                    'and paste the smiles here')
-        if type(self.building_block_smiles) != str:
-            raise TypeError()
-        # with RDkit it could also be checked if it is a valid smiles and if the molecule makes sense
-
-    @property
-    def experiment_id(self):
-        return self._experiment_id
-
-    @experiment_id.setter
-    def experiment_id(self, exp_id: int):
-        if not self.experiment_id or not exp_id:
-            self._experiment_id = exp_id
-        else:
-            # for now
-            print(f'You are trying to change the ID of {self._experiment_id} to {exp_id}. Don\'t do that')
-
-    @property
-    def stock_concentration_donor(self):
-        return self._stock_concentration_donor
-
-    @property
-    def stock_concentration_activator(self):
-        return self._stock_concentration_activator
-
-    @property
-    def stock_concentration_quencher(self):
-        return self._stock_concentration_quencher
-
-    @property
-    def reactor_volumes(self):
-        return self._reactor_volumes
-
-    @property
-    def quencher_equivalents(self):
-        return self._quencher_equivalents
-
-    @property
-    def chromatogram(self):
-        from pandas import read_json
-        try:
-            return read_json(self._chromatogram)
-        except ValueError:
-            return self._chromatogram
-
-    @chromatogram.setter
-    def chromatogram(self, path: str):
-        # Damn, if several detectors are exported, this creates several header lines
-        if not self.chromatogram:
-            try:
-                # several times, I observed pandas.errors.EmptyDataError: No columns to parse from file. The file was
-                # checked manually and fine: therefore the sleep
-                sleep(1)
-                self._chromatogram = read_csv(Path(path), header=16, sep='\t').to_json()
-            except errors.ParserError:
-                raise errors.ParserError('Please make sure your Analytical data is compliant with pandas.read_csv')
-        else:
-            print('attempt was made to overwrite chromatogram, check code or stop doing this')
 
 
 class FlowConditions:
