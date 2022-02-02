9b7445ee5dfb8f8b21503ec4dd654b298a24dedb
Dario Cambié <dario.cambie@mpikg.mpg.de>
2022-02-02 21:26:45 +0100

    minor changes ci

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-""" CLI should be used for device tree initialization from config. """
-from flowchem.cli import main
-
-
-def test_main(capsys):
-    """ Stub for CLI entrypoint """
-    main()
-    stdout, errout = capsys.readouterr()
-    assert "Here" in stdout
