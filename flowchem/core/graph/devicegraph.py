66fdac54982c9b364d334a737028e3e34ea3230e
Dario Cambié <dario.cambie@mpikg.mpg.de>
2022-03-11 11:03:32 +0100

    -deviceGraph.to_yaml()
    No need for this actually

--- flowchem/core/graph/devicegraph.py
+++ flowchem/core/graph/devicegraph.py
@@ -32,21 +32,6 @@ class DeviceGraph:
         # NetworkX Multi directed graph object
         self.graph: nx.MultiDiGraph = nx.MultiDiGraph()
 
-    def to_yml(self):
-        """Convert the graph to a YAML string"""
-        import yaml
-
-        exported_graph = dict()
-        exported_graph["version"] = 1.0
-        exported_graph["devices"] = {
-            device.name: device.to_yml() for device in self.graph.nodes
-        }
-        exported_graph["physical_connections"] = {
-            device.name: device.to_yml() for device in self.graph.edges
-        }
-        # FIXME
-        return yaml.dump(exported_graph)
-
     def add_device(self, device: Any):
         """Add a device or list of devices to the graph"""
 
