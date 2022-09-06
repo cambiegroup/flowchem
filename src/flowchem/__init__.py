2704025982bdc4b34a536ef5ed39cd1bcc6608ee
Dario Cambié <dario.cambie@mpikg.mpg.de>
2022-09-06 15:17:34 +0200

    Do not export everything to package toplevel

--- src/flowchem/__init__.py
+++ src/flowchem/__init__.py
@@ -1,15 +1,9 @@
-""" HW device, organized by manufacturer. """
-from .devices.Hamilton import *
-from .devices.Harvard_Apparatus import *
-from .devices.Huber import *
-from .devices.Knauer import *
-from .devices.Magritek import *
-from .devices.Manson import *
-from .devices.MettlerToledo import *
-from .devices.Phidgets import *
-from .devices.Vapourtec import *
-from .devices.ViciValco import *
+# Single-sourcing version, Option 5 in https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+from importlib.metadata import PackageNotFoundError, version
 
-# Option 5 in https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
-from importlib import metadata
-__version__ = metadata.version('flowchem')
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
