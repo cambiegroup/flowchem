6fe877c7b232ff9cd267c9af3d364d5e2f9fc484
Dario Cambié <dario.cambie@mpikg.mpg.de>
2022-09-21 01:28:29 +0200

    Fix CLI

--- src/flowchem/cli.py
+++ src/flowchem/cli.py
@@ -1,17 +1,24 @@
 """ Shell script executor """
+from pathlib import Path
+
 import rich_click as click
+import uvicorn
+from flowchem import __version__
+from flowchem.server.api_server import create_server_from_file
+from loguru import logger
 
 
 @click.argument("device_config", type=click.Path(), required=True)
 @click.version_option()
 @click.command()
 def main(device_config):
-    """
-    Flowchem device manager.
-    Starts the flowchem server for the devices described in the DEVICE_CONFIG.
-    """
-    print(f"Starting flowchem server with configuration file: '{device_config}'")
-    # FIXME DO STUFF
+    """Flowchem CLI entry point."""
+    print(f"Starting flowchem v. {__version__}!")
+    logger.add(Path("./flowchem.log"))
+    logger.debug(f"Starting server with configuration file: '{device_config}'")
+
+    myapp = create_server_from_file(Path(device_config))
+    uvicorn.run(myapp)
 
 
 if __name__ == "__main__":
