721691eee35b987e148cb2d2a20b70bf1fcddfce
Dario Cambié <dario.cambie@mpikg.mpg.de>
2022-09-21 14:28:46 +0200

    Detect pump type in elite11_finder.py

--- src/flowchem/devices/harvardapparatus/elite11_finder.py
+++ src/flowchem/devices/harvardapparatus/elite11_finder.py
@@ -1,23 +1,26 @@
 """
 This module is used to discover the serial address of any ML600 connected to the PC.
 """
+import asyncio
+import textwrap
+
 import rich_click as click
 import serial.tools.list_ports
-from flowchem.devices.harvardapparatus.elite11 import (
-    HarvardApparatusPumpIO,
-)
+from flowchem.devices.harvardapparatus.elite11 import Elite11InfuseOnly
+from flowchem.devices.harvardapparatus.elite11 import HarvardApparatusPumpIO
 from flowchem.exceptions import InvalidConfiguration
 from loguru import logger
 
 
 # noinspection PyProtectedMember
-def elite11_finder():
+def elite11_finder() -> set:
     """Try to initialize an Elite11 on every available COM port."""
     port_available = [comport.device for comport in serial.tools.list_ports.comports()]
     logger.info(f"Found the following serial port(s): {port_available}")
 
     # Ports connected to an elite11-looking device
     valid_ports = set()
+    config = ""
 
     for serial_port in port_available:
         logger.info(f"Looking for pump on {serial_port}...")
@@ -31,14 +34,36 @@ def elite11_finder():
 
                 pump = link._serial.readline().decode("ascii")
                 try:
-                    int(pump[0:2])
-                    logger.info(f"Pump address is {pump[0:2]}!")
+                    address = int(pump[0:2])
                 except ValueError:
-                    logger.info(f"Single pump, not part of daisy chain.")
+                    address = 0
+
+                test_pump = Elite11InfuseOnly(
+                    link, diameter="20 mm", syringe_volume="10 ml", address=address
+                )
+                info = asyncio.run(test_pump.pump_info())
+
+                p_type = (
+                    "Elite11InfuseOnly" if info.infuse_only else "Elite11InfuseWithdraw"
+                )
+
+                config += textwrap.dedent(
+                    f"""
+                [device.my-elite11-pump{len(valid_ports)}]
+                type = "{p_type}"
+                port = "{serial_port}"
+                address = {address}
+                diameter = "XXX mm"
+                syringe_volume = "YYY ml"
+
+                """
+                )
             else:
                 logger.debug(f"No pump found on {serial_port}")
         except InvalidConfiguration:
             pass
+
+    logger.info(f"Pump configuration stub:\n{config}")
 
     return valid_ports
 
