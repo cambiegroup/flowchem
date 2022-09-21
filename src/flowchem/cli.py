ac41c934814029623a68b78bf916f47e2220114d
Dario Cambié <dario.cambie@mpikg.mpg.de>
2022-09-21 21:34:56 +0200

    Add option for log to file

--- src/flowchem/cli.py
+++ src/flowchem/cli.py
@@ -8,16 +8,22 @@ from flowchem.server.api_server import create_server_from_file
 from loguru import logger
 
 
-@click.argument("device_config", type=click.Path(), required=True)
+@click.argument("device_config_file", type=click.Path(), required=True)
+@click.option(
+    "-l", "--log", "logfile", type=click.Path(), default=None, help="Save logs to file."
+)
 @click.version_option()
 @click.command()
-def main(device_config):
-    """Flowchem CLI entry point."""
-    print(f"Starting flowchem v. {__version__}!")
-    logger.add(Path("./flowchem.log"))
-    logger.debug(f"Starting server with configuration file: '{device_config}'")
+def main(device_config_file, logfile):
+    """Flowchem main program.
 
-    myapp = create_server_from_file(Path(device_config))
+    Parse DEVICE_CONFIG_FILE and starts a server exposing the devices via RESTful API."""
+    print(f"Starting flowchem v. {__version__}!")
+    if logfile:
+        logger.add(Path(logfile))
+    logger.debug(f"Starting server with configuration file: '{device_config_file}'")
+
+    myapp = create_server_from_file(Path(device_config_file))
     uvicorn.run(myapp)
 
 
