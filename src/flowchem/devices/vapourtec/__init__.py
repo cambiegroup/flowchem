ab4de07a79fe316f619250cc29c7d0bb42610ff3
Dario Cambie <dario.cambie@mpikg.mpg.de>
2022-09-22 14:14:39 +0200

    -R4Heater (moved to plugin)

--- src/flowchem/devices/vapourtec/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Vapourtec devices """
-try:
-    from .r4_heater import R4Heater
-
-    __all__ = ["R4Heater"]
-except PermissionError:
-    print("Vapourtec devices disabled - no command description found.")
