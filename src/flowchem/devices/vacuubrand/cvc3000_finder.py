321e8343200c7c12d242d583fe17d5474d549971
Dario Cambie <dario.cambie@mpikg.mpg.de>
2022-11-23 13:19:45 +0100

    Fix autofinder

--- src/flowchem/devices/vacuubrand/cvc3000_finder.py
+++ src/flowchem/devices/vacuubrand/cvc3000_finder.py
@@ -9,27 +9,28 @@ from flowchem.exceptions import InvalidConfiguration
 
 
 # noinspection PyProtectedMember
-def cvc3000_finder(serial_port) -> set[str]:
+def cvc3000_finder(serial_port) -> list[str]:
     """Try to initialize a CVC3000 on every available COM port."""
     logger.debug(f"Looking for CVC3000 on {serial_port}...")
 
     try:
         cvc = CVC3000.from_config(port=serial_port)
     except InvalidConfiguration:
-        return set()
+        return []
 
     try:
         asyncio.run(cvc.initialize())
     except InvalidConfiguration:
         cvc._serial.close()
-        return set()
+        return []
 
-    logger.info(f"CVC3000 ver. {cvc.metadata.version} found on <{serial_port}>")
+    logger.info(f"CVC3000 {cvc.metadata.version} found on <{serial_port}>")
 
-    return set(
+    return [
         dedent(
-            f"""\n\n[device.cvc-{cvc._device_sn}]
-    type = "CVC3000"
-    port = "{serial_port}"\n"""
+            f"""
+            [device.cvc-{cvc._device_sn}]
+            type = "CVC3000"
+            port = "{serial_port}"\n\n"""
         )
-    )
+    ]
