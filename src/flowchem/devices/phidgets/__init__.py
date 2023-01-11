c75172c135847e922fafc68755e908c4aba4ed3e
Wei-Hsin Hsu <cynthiabour@gmail.com>
2023-01-11 09:17:36 +0100

    bubble sensor elements update

--- src/flowchem/devices/phidgets/__init__.py
+++ src/flowchem/devices/phidgets/__init__.py
@@ -1,4 +1,5 @@
 """Phidget-based devices."""
 from .pressure_sensor import PhidgetPressureSensor
+from .bubble_sensor import PhidgetBubbleSensor, PhidgetBubbleSensor_power
 
-__all__ = ["PhidgetPressureSensor"]
+__all__ = ["PhidgetPressureSensor", "PhidgetBubbleSensor", "PhidgetBubbleSensor_power"]
