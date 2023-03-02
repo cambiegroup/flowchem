2e149335b86b912a022c23c9a4665ae8d75bc5ba
Wei-Hsin Hsu <cynthiabour@gmail.com>
2023-03-02 09:40:42 +0100

    add new device, Bronkhorst digital electronic pressure meters and pressure controllers

--- src/flowchem/devices/bronkhorst/el_flow_component.py
+++ src/flowchem/devices/bronkhorst/el_flow_component.py
@@ -2,11 +2,42 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from flowchem import ureg
 from flowchem.components.base_component import FlowchemComponent
+from flowchem.components.sensors.pressure import PressureSensor
 from flowchem.devices.flowchem_device import FlowchemDevice
 
 if TYPE_CHECKING:
-    from .el_flow import MFC
+    from .el_flow import MFC, EPC
+
+class EPCComponent(PressureSensor):
+    hw_device: EPC  # just for typing
+
+    def __init__(self, name: str, hw_device: FlowchemDevice):
+        """A generic power supply"""
+        super().__init__(name, hw_device)
+        self.add_api_route("/set-flow-rate", self.get_pressure, methods=["GET"])
+        self.add_api_route("/stop", self.stop, methods=["PUT"])
+        self.add_api_route("/set-pressure", self.set_pressure_setpoint, methods=["PUT"])
+
+    async def read_pressure(self, units: str = "bar"):
+        """Read from sensor, result to be expressed in units."""
+        p = await self.hw_device.get_pressure()
+        return p * ureg(units)
+
+    async def set_pressure_setpoint(self, pressure: str) -> bool:
+        """Set controlled pressure to the instrument; default unit: bar"""
+        await self.hw_device.set_pressure(pressure)
+        return True
+
+    async def get_pressure(self) -> float:
+        """get current system pressure in bar"""
+        return await self.hw_device.get_pressure()
+
+    async def stop(self) -> bool:
+        """Stop pressure controller"""
+        await self.hw_device.set_pressure("0 bar")
+        return True
 
 
 class MFCComponent(FlowchemComponent):
@@ -20,7 +51,7 @@ class MFCComponent(FlowchemComponent):
         self.add_api_route("/set-flow-rate", self.set_flow_setpoint, methods=["PUT"])
 
     async def set_flow_setpoint(self, flowrate: str) -> bool:
-        """Set flow rate to the instrument; default unit: ul/min"""
+        """Set flow rate to the instrument; default unit: ml/min"""
         await self.hw_device.set_flow_setpoint(flowrate)
         return True
 
