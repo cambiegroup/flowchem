0fbd0626565f95baa9ef890af7d9f7142cb8904e
Dario Cambie <dario.cambie@mpikg.mpg.de>
2023-05-30 16:49:51 +0200

    Skip test_autodiscover_cli only under CI

--- tests/cli/test_autodiscover_cli.py
+++ tests/cli/test_autodiscover_cli.py
@@ -1,11 +1,18 @@
 import pytest
+import os
 from click.testing import CliRunner
 
 from flowchem.utils.autodiscover import main
 
 
-@pytest.mark.skip(reason="Fails in CI due to network limits on broadcast")
-def test_autodiscover_cli(mocker):
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
+
+@pytest.mark.skipif(
+    IN_GITHUB_ACTIONS,
+    reason="Test doesn't work in Github Actions since broadcast is not allowed.",
+)
+def test_autodiscover_cli():
     runner = CliRunner()
 
     with runner.isolated_filesystem():
