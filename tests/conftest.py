fde1b1b35cdc2123c293302a73bb0915687b8c61
Dario Cambie <dario.cambie@mpikg.mpg.de>
2023-07-13 20:21:51 +0200

    Fix timeout in GA

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,9 +11,9 @@ def flowchem_test_instance(xprocess):
     main = Path(__file__).parent.resolve() / ".." / "src" / "flowchem" / "__main__.py"
 
     class Starter(ProcessStarter):
-        # Process startup ends with this text in stdout
+        # Process startup ends with this text in stdout (timeout is long cause some GA runners are slow)
         pattern = "Uvicorn running"
-        timeout = 10
+        timeout = 30
 
         # execute flowchem with current venv
         args = [sys.executable, main, config_file]
