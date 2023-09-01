1085f144cb7b7183f789d05041b7b09cd1639791
Dario Cambie <dario.cambie@mpikg.mpg.de>
2023-09-01 16:36:03 +0200

    Update config parser test

--- tests/server/test_config_parser.py
+++ tests/server/test_config_parser.py
@@ -4,7 +4,11 @@ from textwrap import dedent
 import pytest
 from flowchem_test.fakedevice import FakeDevice
 
-from flowchem.server.configuration_parser import parse_config
+from flowchem.server.configuration_parser import (
+    parse_config,
+    ensure_device_name_is_valid,
+    instantiate_device_from_config,
+)
 from flowchem.utils.exceptions import InvalidConfigurationError
 
 
@@ -20,11 +24,29 @@ def test_minimal_valid_config():
     cfg = parse_config(cfg_txt)
     assert "filename" in cfg
     assert "device" in cfg
-    assert isinstance(cfg["device"].pop(), FakeDevice)
 
 
-def test_name_too_long():
-    cfg_txt = BytesIO(b"""[device.this_name_is_too_long_and_should_be_shorter]""")
+def test_device_instantiation():
+    cfg_txt = BytesIO(
+        dedent(
+            """
+            [device.test-device]
+            type = "FakeDevice"
+            """,
+        ).encode("utf-8"),
+    )
+    cfg = parse_config(cfg_txt)
+    devices = instantiate_device_from_config(cfg)
+    assert isinstance(devices.pop(), FakeDevice)
+
+
+def test_device_name_too_long():
     with pytest.raises(InvalidConfigurationError) as excinfo:
-        parse_config(cfg_txt)
+        ensure_device_name_is_valid("this_name_is_too_long_and_should_be_shorter")
     assert "too long" in str(excinfo.value)
+
+
+def test_device_name_with_dot():
+    with pytest.raises(InvalidConfigurationError) as excinfo:
+        ensure_device_name_is_valid("this.name")
+    assert "Invalid character" in str(excinfo.value)
