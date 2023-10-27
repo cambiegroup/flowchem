5673b8ea348d33134f18540e0a891e5663bbca56
jwolf <jakob.wolf@mpikg.mpg.de>
2023-10-27 11:02:49 +0200

    add id and meaningful setting and parsing

--- flowchem/devices/Knauer/Autosampler.py
+++ flowchem/devices/Knauer/Autosampler.py
@@ -101,13 +101,15 @@ class KnauerAS(ASEthernetDevice):
     Class to control Knauer or basically any Spark Holland AS.
 
     """
-
-    def __init__(self, ip_address, port=ASEthernetDevice.UDP_PORT, buffersize=ASEthernetDevice.BUFFER_SIZE):
+    AS_ID = 61
+    def __init__(self,ip_address,  autosampler_id = None, port=ASEthernetDevice.TCP_PORT, buffersize=ASEthernetDevice.BUFFER_SIZE):
 
         super().__init__(ip_address, port, buffersize)
         # get statuses, that is basically syringe syize, volumes, platetype
 
-    def communicate(self, message: str):
+        self.autosampler_id = autosampler_id if autosampler_id else KnauerAS.AS_ID
+
+    def autosampler_set(self, message: str or int):
         """
         Sends command and receives reply, deals with all communication based stuff and checks that the valve is
         of expected type
@@ -115,358 +117,63 @@ class KnauerAS(ASEthernetDevice):
         :return: reply: str
         """
         reply = super()._send_and_receive(message)
-        # reply digest
 
-        # verify that reply start and end are alright
+        # this only checks that it was acknowledged
+        self._parse_setting_reply(reply)
 
-        # verify that reply is present in
-        if reply in NDA_knauer_AS.knauer_AS.ASReplies:
-            # retry once
-
-            if reply == NDA_knauer_AS.knauer_AS.ASReplies.NOT_ACKNOWLEDGE:
-                ASError()
-        elif 1:
-            # do true message digest
-            pass
-        else:
-            # message is neither simple reply nor an extended one -> bullshit reply
-            raise ASError
-            pass
-            return reply
-
-    def get_current_position(self):
-        curr_pos = self.communicate("P")
-        logging.debug(f"Current position is {curr_pos}")
-
-        return curr_pos
-
-    def switch_to_position(self, position: int or str):
-        try:
-            position = int(position)
-        except ValueError:
-            pass
-
-        # allows lower and uppercase commands in case of injection
-        if isinstance(position, str):
-            position = position.upper()
-
-        # switching necessary?
-        if position == self._valve_state:
-            logging.debug("already at that position")
-            return
-
-        # change to selected position
-        reply = self.communicate(position)
-
-        # check if this was done
-        if reply == "OK":
-            logging.debug("switching successful")
-            self._valve_state = position
-
-        elif "E0" in reply:
-
-            logging.error("valve was not switched because valve refused")
-            raise SwitchingException("valve was not switched because valve refused")
-
-        elif "E1" in reply:
-            logging.error("Motor current to high. Check that")
-            raise SwitchingException("Motor current to high. Check that")
-
-        else:
-            raise SwitchingException(f"Unknown reply received. Reply is {reply}")
-
-    def get_valve_type(self):
-        """aquires valve type, if not supported will throw error.
-        This also prevents to initialize some device as a KnauerValve"""
-        reply = self.communicate("T")[6:]
-        # could be more pretty by passing expected answer to communicate
-        try:
-            reply = int(reply)
-        except ValueError:
-            pass
-        try:
-            headtype = KnauerValveHeads(reply)
-        except ValueError as e:
-            raise KnauerError(
-                f"It seems you're trying instantiate a unknown device/unknown valve type {e} as Knauer Valve."
-                "Only Valves of type 16, 12, 10 and LI are supported"
-            ) from e
-        logging.info(
-            f"Valve successfully connected, Type is {headtype} at address {self.ip_address}"
-        )
-        return headtype
-
-    def close_connection(self):
-        logging.info(f"Valve at address closed connection {self.ip_address}")
-        self.sock.close()
-
-
-# Read and write, read: command?; write = command:setpoint
-# BEWARE MANUAL STATES 0-50000µml/min HOWEVER this depends on headtype
-FLOW = "FLOW"  # 0-50000 µL/min, int only!
-PMIN10 = "PMIN10"  # 0-400 in 0.1 MPa, use to avoid dryrunning
-PMIN50 = "PMIN50"  # 0-150 in 0.1 MPa, use to avoid dryrunning
-PMAX10 = "PMAX10"  # 0-400 in 0.1 MPa, chosen automatically by selecting pump head
-PMAX50 = "PMAX50"  # 0-150 in 0.1 MPa, chosen automatically by selecting pumphead
-IMIN10 = "IMIN10"  # 0-100 minimum motor current
-IMIN50 = "IMIN50"  # 0-100 minimum motor current
-HEADTYPE = "HEADTYPE"  # 10, 50 ml. Value refers to highest flowrate in ml/min
-STARTLEVEL = "STARTLEVEL"  # 0, 1 configures start. 0 -> only start pump when shorted to GND, 1 -> always allow start
-ERRIO = "ERRIO"  # 0, 1 write/read error in/output ??? sets errio either 1 or 0, reports errio:ok
-STARTMODE = "STARTMODE"  # 0, 1; 0=pause pump after switchon, 1=start immediatley with previous set flow rate
-# no idea what these do...
-ADJ10 = "ADJ10"  # 100-2000
-ADJ50 = "ADJ50"  # 100-2000
-CORR10 = "CORR10"  # 0-300
-CORR50 = "CORR50"  # 0-300
-
-# RD only
-EXTFLOW = "EXTFLOW?"
-IMOTOR = "IMOTOR?"  # motor current in relative units 0-100
-PRESSURE = "PRESSURE?"  # reads the pressure in 0.1 MPa
-ERRORS = "ERRORS?"  # displays last 5 error codes
-
-# WR only
-EXTCONTR = (
-    "EXTCONTR:"  # 0, 1; 1= allows flow control via external analog input, 0 dont allow
-)
-LOCAL = "LOCAL"  # no parameter, releases pump to manual control
-REMOTE = "REMOTE"  # manual param input prevented
-PUMP_ON = "ON"  # starts flow
-PUMP_OFF = "OFF"  # stops flow
-
-
-class KnauerPump(KnauerEthernetDevice):
-    def __init__(
-        self,
-        ip_address,
-        port=None,
-        buffersize=None,
-    ):
-        super().__init__(ip_address, port, buffersize)
-        self.max_pressure, self.max_flow = None, None
-        # Check connection by reading pump head type
-        _ = self.headtype
-
-    def communicate(self, message: str):
+    def autosampler_query(self, message: str or int):
         """
-        sends command and receives reply, deals with all communication based stuff and checks
-        that the valve is of expected type
+        Sends command and receives reply, deals with all communication based stuff and checks that the valve is
+        of expected type
         :param message:
         :return: reply: str
         """
+        reply = super()._send_and_receive(message)
 
-        # beware: I think the pumps want \n\r as end of message, the valves \r\n
-        message = str(message) + "\n\r"
-        reply = super()._send_and_receive_handler(message).rstrip()
-        if "ERROR:1" in reply:
-            CommandError(
-                f"Invalid message sent to device. Message was: {message}. Reply is {reply}"
-            )
+        query_reply = self._parse_query_reply(reply, message)
+        return query_reply
 
-        elif "ERROR:2" in reply:
-            ParameterError(
-                f"Setpoint refused by device. Refer to manual for allowed values.  Message was: '{message}'. "
-                f"Reply is '{reply}'"
-            )
+    def _parse_setting_reply(self, reply):
+        # reply needs to be binary string
+        from NDA_knauer_AS.knauer_AS import CommunicationFlags
 
-        elif ":OK" in reply:
-            logging.info("setpoint successfully set")
+        if reply == CommunicationFlags.ACKNOWLEDGE:
+            return True
+        elif reply == CommunicationFlags.TRY_AGAIN:
+            raise ASBusyError
+        elif reply == CommunicationFlags.NOT_ACKNOWLEDGE:
+            raise CommandOrValueError
+        # this is only the case with replies on queries
+        else:
+            raise ASError(f"The reply is {reply} and does not fit the expected reply for value setting")
 
-        elif message.rstrip()[:-1] + ":" in reply:
-            logging.info(f"setpoint successfully acquired, is {reply}")
-            return reply.split(":")[-1]
-        elif not reply:
-            raise CommandError("No reply received")
-        return reply
+    def _parse_query_reply(self, reply, as_command):
+        from NDA_knauer_AS.knauer_AS import ReplyStructure, KnauerASCommands
+        reply_start_char, reply_stripped, reply_end_char = reply[:ReplyStructure.STX_END.value], \
+                                                           reply[
+                                                           ReplyStructure.STX_END.value:ReplyStructure.ETX_START.value], \
+                                                           reply[ReplyStructure.ETX_START.value:]
+        if reply_start_char != ReplyStructure.MESSAGE_START | reply_end_char != ReplyStructure.MESSAGE_END:
+            raise CommunicationError
 
-    # read and write. write: append ":value", read: append "?"
-    def message_constructor_dispatcher(
-        self, message, setpoint: int = None, setpoint_range: tuple = None
-    ):
 
-        if not setpoint:
-            return self.communicate(message + "?")
-
-        elif setpoint_range:
-            if setpoint in range(*setpoint_range):
-                return self.communicate(message + ":" + str(setpoint))
-
-            else:
-                ParameterError(
-                    f"Internal check shows that setpoint provided ({setpoint}) is not in range ({setpoint_range})."
-                    f"Refer to manual."
-                )
+        # basically, if the device gives an extended reply, length will be 14. This only matters for get commands
+        if len(reply_stripped) == 14:
+        # decompose further
+            as_id = reply_stripped[ReplyStructure.STX_END.value:ReplyStructure.ID_END.value]
+            as_ai = reply_stripped[ReplyStructure.ID_END.value:ReplyStructure.AI_END.value]
+            as_pfc = reply_stripped[ReplyStructure.AI_END.value:ReplyStructure.PFC_END.value]
+            as_val = reply_stripped[ReplyStructure.PFC_END.value:ReplyStructure.VALUE_END.value]
+            # check if reply from requested device
+            if as_id.decode() != self.autosampler_id:
+                raise ASError(f"ID of used AS is {self.autosampler_id}, but ID in reply is as_id")
+            if as_pfc.decode() != as_command[ReplyStructure.AI_END.value:ReplyStructure.PFC_END.value]:
+                raise ASError(f"Reply of AS is for another query. Query was {KnauerASCommands[as_command].name}, but reply is for {KnauerASCommands[as_pfc].name}")
+            return as_val.decode().lstrip("0")
+            # check the device ID against current device id, check that reply is on send request
 
         else:
-            return self.communicate(message + ":" + str(setpoint))
-
-    def set_flow(self, setpoint_in_ml_min: float = None):
-        """
-
-        :param setpoint_in_ml_min: in mL/min
-        :return: nothing
-        """
-        set_flowrate_ul_min = int(setpoint_in_ml_min * 1000)
-        flow = self.message_constructor_dispatcher(FLOW, setpoint=set_flowrate_ul_min,
-            setpoint_range=(0, self.max_flow + 1),
-        )
-        logging.info(
-            f"Flow of pump {self.ip_address} is set to {setpoint_in_ml_min}, returns {flow}"
-        )
-
-    @property
-    def headtype(self):
-        reply = int(self.message_constructor_dispatcher(HEADTYPE)[-2:])
-        try:
-            headtype = KnauerPumpHeads(reply)
-        except ValueError as e:
-            raise KnauerError(
-                "It seems you're trying instantiate an unknown device/unknown pump type as Knauer Pump."
-                "Only Knauer Azura Compact is supported"
-            ) from e
-
-        logging.info(f"Head type of pump {self.ip_address} is {headtype}")
-        self.max_pressure, self.max_flow = (
-            (400, 10000)
-            if headtype == KnauerPumpHeads.FLOWRATE_TEN_ML
-            else (150, 50000)
-        )
-        return headtype
-
-    @headtype.setter
-    def headtype(self, setpoint: KnauerPumpHeads):
-        reply = self.message_constructor_dispatcher(HEADTYPE, setpoint=setpoint.value)
-        self.max_pressure, self.max_flow = (
-            (400, 10000)
-            if setpoint == KnauerPumpHeads.FLOWRATE_TEN_ML
-            else (150, 50000)
-        )
-        logging.info(
-            f"Head type of pump {self.ip_address} is set to {setpoint}, returns {reply}"
-        )
-
-    def set_minimum_pressure(self, pressure_in_bar=None):
-
-        command = PMIN10 if self.headtype == KnauerPumpHeads.FLOWRATE_TEN_ML else PMIN50
-
-        reply = self.message_constructor_dispatcher(
-            command,
-            setpoint=pressure_in_bar,
-            setpoint_range=(0, self.max_pressure + 1),
-        )
-
-        logging.info(
-            f"Minimum pressure of pump {self.ip_address} is set to {pressure_in_bar}, returns {reply}"
-        )
-
-    def set_maximum_pressure(self, pressure_in_bar=None):
-        command = PMAX10 if self.headtype == KnauerPumpHeads.FLOWRATE_TEN_ML else PMAX50
-
-        reply = self.message_constructor_dispatcher(
-            command,
-            setpoint=pressure_in_bar,
-            setpoint_range=(0, self.max_pressure + 1),
-        )
-
-        logging.info(
-            f"Maximum pressure of pump {self.ip_address} is set to {pressure_in_bar}, returns {reply}"
-        )
-
-    def set_minimum_motor_current(self, setpoint=None):
-        command = IMIN10 if self.headtype == KnauerPumpHeads.FLOWRATE_TEN_ML else IMIN50
-
-        reply = self.message_constructor_dispatcher(
-            command, setpoint=setpoint, setpoint_range=(0, 101)
-        )
-        logging.info(
-            f"Minimum motor current of pump {self.ip_address} is set to {setpoint}, returns {reply}"
-        )
-
-    def set_start_level(self, setpoint=None):
-        reply = self.message_constructor_dispatcher(
-            STARTLEVEL, setpoint=setpoint, setpoint_range=(0, 2)
-        )
-        logging.info(
-            f"Start level of pump {self.ip_address} is set to {setpoint}, returns {reply}"
-        )
-
-    def set_start_mode(self, setpoint=None):
-        """
-
-        :param setpoint: 0 pause pump after switch on. 1 switch on immediately with previously selected flow rate
-        :return: device message
-        """
-        if setpoint in (0, 1):
-            reply = self.message_constructor_dispatcher(STARTMODE, setpoint=setpoint)
-            logging.info(
-                f"Start mode of pump {self.ip_address} is set to {setpoint}, returns {reply}"
-            )
-        else:
-            logging.warning("Supply binary value")
-
-    def set_adjusting_factor(self, setpoint: int = None):
-        command = ADJ10 if self.headtype == KnauerPumpHeads.FLOWRATE_TEN_ML else ADJ50
-        reply = self.message_constructor_dispatcher(
-            command, setpoint=setpoint, setpoint_range=(0, 2001)
-        )
-        logging.info(
-            f"Adjusting factor of pump {self.ip_address} is set to {setpoint}, returns {reply}"
-        )
-
-    def set_correction_factor(self, setpoint=None):
-        command = CORR10 if self.headtype == KnauerPumpHeads.FLOWRATE_TEN_ML else CORR50
-        reply = self.message_constructor_dispatcher(
-            command, setpoint=setpoint, setpoint_range=(0, 301)
-        )
-        logging.info(
-            f"Correction factor of pump {self.ip_address} is set to {setpoint}, returns {reply}"
-        )
-
-    # read only
-    def read_pressure(self):
-        reply = int(self.communicate(PRESSURE))
-        logging.info(f"Pressure reading of pump {self.ip_address} returns {reply} bar")
-        return reply
-
-    def read_extflow(self):
-        reply = int(self.communicate(EXTFLOW))
-        logging.info(f"Extflow reading of pump {self.ip_address} returns {reply}")
-        return reply
-
-    def read_errors(self):
-        reply = self.communicate(ERRORS)
-        logging.info(f"Error reading of pump {self.ip_address} returns {reply}")
-        return reply
-
-    def read_motor_current(self):
-        reply = int(self.communicate(IMOTOR))
-        logging.info(f"Motor current reading of pump {self.ip_address} returns {reply} A")
-        return reply
-
-    # TODO run flag
-    # write only
-    def start_flow(self):
-        self.communicate(PUMP_ON)
-        logging.info("Pump switched on")
-
-    def stop_flow(self):
-        self.communicate(PUMP_OFF)
-        logging.info("Pump switched off")
-
-    def set_local(self, param: int):
-        if param in (0, 1):
-            logging.info(f"Pump {self.ip_address} set local {param}")
-            self.communicate(LOCAL + ":" + str(param))
-        else:
-            logging.warning("Supply binary value")
-
-    def set_remote(self, param: int):
-        if param in (0, 1):
-            logging.info(f"Pump {self.ip_address} set remote {param}")
-            self.communicate(REMOTE + ":" + str(param))
-        else:
-            logging.warning("Supply binary value")
+            raise ASError(f"AS reply did not fit any of the known patterns, reply is: {reply_stripped}")
 
 
 
