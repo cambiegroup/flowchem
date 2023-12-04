5cf8ed70f179438f3b977e5e8e1650469aee7d77
magz97 <miguelgarciazubillaga@gmail.com>
2023-12-04 10:24:46 +0100

    correct the position mapping for single syringe pump
    (cherry picked from commit 54ca9342bdca8823a8bcd340aed26e6053c2c240)

--- src/flowchem/devices/hamilton/ml600_valve.py
+++ src/flowchem/devices/hamilton/ml600_valve.py
@@ -5,37 +5,31 @@ from typing import TYPE_CHECKING
 
 from loguru import logger
 
-from flowchem.components.valves.distribution_valves import TwoPortDistribution
+from flowchem.components.valves.distribution_valves import TwoPortDistributionValve
 
 if TYPE_CHECKING:
     from .ml600 import ML600
 
 
-class ML600Valve(TwoPortDistribution):
+class ML600Valve(TwoPortDistributionValve):
     hw_device: ML600  # for typing's sake
 
-    # position_mapping = {
-    #     "input": "1",  # 9 is default inlet, i.e. 1
-    #     "output": "3",  # 10 is default outlet, i.e. 3
-    # }
-    def __init__(self, name: str, hw_device: ML600, valve_code: str = ""):
-        """Create a ValveControl object."""
-        super().__init__(name, hw_device)
-        self.valve_code = valve_code   #"left:B, right:C"
+    position_mapping = {
+        "input": "1",  # 9 is default inlet, i.e. 1
+        "output": "3",  # 10 is default outlet, i.e. 3
+    }
 
     async def set_position(self, position: str) -> bool:
         """Set pump to position."""
-        # await super().set_position(position)  # Validation
+        await super().set_position(position)  # Validation
         return await self.hw_device.set_valve_position(
             target_position=ML600Valve.position_mapping[position],
-            valve_code=self.valve_code,
             wait_for_movement_end=True,
-
         )
 
     async def get_position(self) -> str:
-        """Current pump position."""
-        pos = await self.hw_device.get_valve_position(valve_code=self.valve_code,)
+        """Get current pump position."""
+        pos = await self.hw_device.get_valve_position()
         reverse_position_mapping = {
             v: k for k, v in ML600Valve.position_mapping.items()
         }
