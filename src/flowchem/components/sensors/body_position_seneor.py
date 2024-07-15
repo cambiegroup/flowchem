7fcf26b234b30b450964f690c82297d6178b1c46
Wei-Hsin Hsu <cynthiabour@gmail.com>
2024-07-15 20:58:41 +0200

    change the function name for general usage

--- src/flowchem/components/sensors/body_position_seneor.py
+++ src/flowchem/components/sensors/body_position_seneor.py
@@ -9,13 +9,13 @@ class BodySensor(Sensor):
 
     def __init__(self, name: str, hw_device: FlowchemDevice) -> None:
         super().__init__(name, hw_device)
-        self.add_api_route("/read_position", self.read, methods=["GET"])
+        self.add_api_route("/position", self.get_position, methods=["GET"])
 
         # Ontology: Pressure Sensor Device
         self.component_info.owl_subclass_of.append(
             "http://purl.obolibrary.org/obo/NCIT_C190586",
         )
 
-    async def read(self):
+    async def get_position(self):
         """Read from sensor, result to be expressed in units (optional)."""
         ...
