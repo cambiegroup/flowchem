4b77e5aa6696ae905505385e8a826a245de3e8e8
Wei-Hsin Hsu <cynthiabour@gmail.com>
2024-07-22 14:52:06 +0200

    correct the output type and delete the unused function

--- src/flowchem/devices/oceanoptics/flame.py
+++ src/flowchem/devices/oceanoptics/flame.py
@@ -82,7 +82,7 @@ class FlameOptical(FlowchemDevice):
             return [i / self.max_intensity for i in i_list]
 
     async def get_wavelength(self):
-        return self.wavelengths
+        return self.wavelengths.tolist()
 
     async def integration_time(self, int_time: int):
         """
@@ -100,45 +100,6 @@ class FlameOptical(FlowchemDevice):
         else:
             logger.warning("the input integration time higher than the limit. set the highest integration time int")
             self.spectrometer.integration_time_micros(h)
-
-
-def all_usb_devices_by_usb():
-    import usb
-    from usb import backend
-
-    # find all usb devices
-    # next(Path(libusb.__file__).parent.rglob('x64/libusb-1.0.dll'))
-    # backend = usb.backend.libusb1.get_backend(find_library=lambda x: r"'C:/Users/BS-Flowlab/PycharmProjects/flowchem/venv/lib/site-packages/libusb/_platform/_windows/x64/libusb-1.0.dll'")  # adapt to your path
-    # usb_devices = usb.core.find(backend=backend, find_all=True)
-    # usb_devices = usb.core.find(find_all=True)
-    # for usb_device in usb_devices:
-    #     print(usb_device)
-
-    # find our device
-    dev = usb.core.find(idVendor=0x2457, idProduct=0x1022)
-    # was it found?
-    if dev is None:
-        raise ValueError('Device not found')
-
-    # set the active configuration. With no arguments, the first
-    # configuration will be the active one
-    dev.set_configuration()
-
-    # get an endpoint instance
-    cfg = dev.get_active_configuration()
-    intf = cfg[(0, 0)]
-    ep1, ep2, ep3, ep4 = intf.endpoints()
-    assert [ep1, ep2, ep3, ep4] is not None
-
-    # ep1 = usb.util.find_descriptor(
-    #     intf,
-    #     # match the first OUT endpoint
-    #     custom_match=lambda e: usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_OUT)
-    #
-    # ep_spc = usb.util.find_descriptor(
-    #     intf,
-    #     # match the first IN endpoint
-    #     custom_match=lambda e: usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_IN)
 
 
 async def main():
