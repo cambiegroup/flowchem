93df234134a117c2495dcac72d638b2f2254835c
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2024-07-23 17:04:15 +0200

    Add the names in the authorship

--- src/flowchem/utils/people.py
+++ src/flowchem/utils/people.py
@@ -1,8 +1,9 @@
 from pydantic import NameEmail
 
-__all__ = ["dario", "jakob", "wei_hsin"]
+__all__ = ["dario", "jakob", "wei_hsin", "Samuel_Saraiva"]
 
 
 dario = NameEmail(name="Dario Cambiè", email="2422614+dcambie@users.noreply.github.com")
 jakob = NameEmail(name="Jakob Wolf", email="Jakob.Wolf@mpikg.mpg.de")
 wei_hsin = NameEmail(name="Wei-Hsin Hsu", email="Wei-hsin.Hsu@mpikg.mpg.de")
+Samuel_Saraiva = NameEmail(name="Samuel Saraiva", email="Samuel.Saraiva@mpikg.mpg.de")
