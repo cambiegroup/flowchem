63bc0911f52d7db136f08d8b9e56696bf0233f09
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2024-08-06 11:05:52 +0200

    Add docstring to better know how the pytest work

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,24 @@ from xprocess import ProcessStarter
 
 @pytest.fixture(scope="module")
 def flowchem_test_instance(xprocess):
+    """
+    Pytest fixture to set up and tear down a FlowChem instance for testing.
+
+    This fixture uses the xprocess plugin to start a FlowChem instance before any tests
+    in the module run and ensures it is terminated after all tests complete. The fixture
+    expects a configuration file named 'test_config.toml' in the same directory as the
+    test script and the main FlowChem script to be located at '../src/flowchem/__main__.py'
+    relative to the test script.
+
+    Args:
+        xprocess: The pytest-xprocess fixture, used to manage external processes.
+
+    Yields:
+        None. The fixture sets up the FlowChem instance before tests and tears it down after tests.
+
+    Raises:
+        TimeoutError: If the FlowChem instance does not start within the specified timeout.
+    """
     config_file = Path(__file__).parent.resolve() / "test_config.toml"
     main = Path(__file__).parent.resolve() / ".." / "src" / "flowchem" / "__main__.py"
 
@@ -21,3 +39,4 @@ def flowchem_test_instance(xprocess):
     xprocess.ensure("flowchem_instance", Starter)
     yield
     xprocess.getinfo("flowchem_instance").terminate()
+
