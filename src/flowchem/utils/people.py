aac26618ca65fc0131944d52d3e3dfdab24e6a60
mgarcia <miguel.garcia@mpikg.mpg.de>
2024-08-19 17:08:32 +0200

    Added knauer_autosampler.py and knauer_autosampler_component.py scripts to knauer module. Containing just structure of script.

--- src/flowchem/utils/people.py
+++ src/flowchem/utils/people.py
@@ -7,3 +7,4 @@ dario = NameEmail(name="Dario Cambiè", email="2422614+dcambie@users.noreply.git
 jakob = NameEmail(name="Jakob Wolf", email="Jakob.Wolf@mpikg.mpg.de")
 wei_hsin = NameEmail(name="Wei-Hsin Hsu", email="Wei-hsin.Hsu@mpikg.mpg.de")
 Samuel_Saraiva = NameEmail(name="Samuel Saraiva", email="Samuel.Saraiva@mpikg.mpg.de")
+miguel = NameEmail(name="Miguel Garcia", email="Miguel.Garcia@mpikg.mpg.de")
