56e1b0e81ffe2ce6da85d372e8d68ac3f550f588
mgarcia <miguel.garcia@mpikg.mpg.de>
2024-08-20 15:47:45 +0200

    Added Miguel to people.py

--- src/flowchem/utils/people.py
+++ src/flowchem/utils/people.py
@@ -6,5 +6,6 @@ __all__ = ["dario", "jakob", "wei_hsin", "Samuel_Saraiva"]
 dario = NameEmail(name="Dario Cambiè", email="2422614+dcambie@users.noreply.github.com")
 jakob = NameEmail(name="Jakob Wolf", email="Jakob.Wolf@mpikg.mpg.de")
 wei_hsin = NameEmail(name="Wei-Hsin Hsu", email="Wei-hsin.Hsu@mpikg.mpg.de")
+miguel = NameEmail(name="Miguel Garcia", email="miguel.garcia@mpikg.mpg.de")
 Samuel_Saraiva = NameEmail(name="Samuel Saraiva", email="Samuel.Saraiva@mpikg.mpg.de")
 miguel = NameEmail(name="Miguel Garcia", email="Miguel.Garcia@mpikg.mpg.de")
