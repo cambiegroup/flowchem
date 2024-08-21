3bceaee345ea9c730ed71e4c282ed60f6418a40d
mgarcia <miguel.garcia@mpikg.mpg.de>
2024-08-21 10:44:29 +0200

    Added jakob to authors.

--- src/flowchem/devices/knauer/knauer_autosampler.py
+++ src/flowchem/devices/knauer/knauer_autosampler.py
@@ -1,6 +1,6 @@
 from flowchem.devices.flowchem_device import FlowchemDevice
 from flowchem.components.device_info import DeviceInfo
-from flowchem.utils.people import Samuel_Saraiva, miguel
+from flowchem.utils.people import jakob, Samuel_Saraiva, miguel
 from flowchem.devices.knauer._common import KnauerEthernetDevice
 
 from flowchem.devices.knauer.knauer_autosampler_component import (
@@ -15,7 +15,7 @@ from loguru import logger
 class KnauerAutosampler(FlowchemDevice):
     """Autosampler control class."""
     device_info = DeviceInfo(
-        authors=[Samuel_Saraiva],
+        authors=[jakob, Samuel_Saraiva, miguel],
         maintainers=[Samuel_Saraiva],
         manufacturer="Knauer",
         model="Autosampler",
@@ -24,7 +24,7 @@ class KnauerAutosampler(FlowchemDevice):
     def __init__(self, name, another_attribute: str = "some_attribute"):
         super().__init__(name)
         self.device_info = DeviceInfo(
-            authors=[Samuel_Saraiva],
+            authors=[jakob, Samuel_Saraiva, miguel],
             maintainers=[Samuel_Saraiva],
             manufacturer="Knauer",
             model="Autosampler",
