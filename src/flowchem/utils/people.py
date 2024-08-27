7708c76b9bd3e514182380a57d682403b177d283
mgarcia <miguel.garcia@mpikg.mpg.de>
2024-08-27 09:44:24 +0200

    Added command class

--- src/flowchem/utils/people.py
+++ src/flowchem/utils/people.py
@@ -1,11 +1,9 @@
 from pydantic import NameEmail
 
-__all__ = ["dario", "jakob", "wei_hsin", "samuel_saraiva", "miguel"]
+__all__ = ["dario", "jakob", "wei_hsin"]
 
 
 dario = NameEmail(name="Dario Cambiè", email="2422614+dcambie@users.noreply.github.com")
 jakob = NameEmail(name="Jakob Wolf", email="Jakob.Wolf@mpikg.mpg.de")
 wei_hsin = NameEmail(name="Wei-Hsin Hsu", email="Wei-hsin.Hsu@mpikg.mpg.de")
 miguel = NameEmail(name="Miguel Garcia", email="miguel.garcia@mpikg.mpg.de")
-samuel_saraiva = NameEmail(name="Samuel Saraiva", email="Samuel.Saraiva@mpikg.mpg.de")
-
