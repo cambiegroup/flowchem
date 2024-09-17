45bd4c239f4562bf77e01a532e7af822044af803
bs-flowlab <jakob.wolf@mpikg.mpg.de>
2024-09-17 14:40:44 +0200

    erased tray_layout attribute. It should be from the autosampler itself.

--- src/flowchem/components/cnc/cnc.py
+++ src/flowchem/components/cnc/cnc.py
@@ -8,17 +8,14 @@ class CNC(FlowchemComponent):
     A generic CNC device that controls movement in 3 dimensions (X, Y, Z).
     """
 
-    def __init__(self, name: str, hw_device: FlowchemDevice,  tray_layout) -> None:
+    def __init__(self, name: str, hw_device: FlowchemDevice) -> None:
         super().__init__(name, hw_device)
         self.add_api_route("/set_xy_position", self.set_xy_position, methods=["PUT"])
         self.add_api_route("/set_z_position", self.set_z_position, methods=["PUT"])
         self.add_api_route("/get_position", self.get_position, methods=["GET"])
         self.component_info.type = "cnc"
-        self.tray_layout = tray_layout  # Dictionary to hold tray configurations
-        self.current_tray = 0  # The default tray is Tray 0
-        self.current_position = (0, 0)  # Start at the origin of Tray 0 (0, 0)
 
-    async def set_xy_position(self, tray: int = 0, row: int = 0, column: int = 0) -> None:
+    async def set_xy_position(self, plate: str = "", row: int = 0, column: int = 0) -> None:
         """
         Move the CNC device to the specified (x, y) coordinate.
         """
