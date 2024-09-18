a22623363e6e2ed1271c7dbf5d49c92bab3d61ae
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2024-09-18 11:01:33 +0200

    Allow inspect the get and put methods available in the API more easily

--- src/flowchem/components/component_info.py
+++ src/flowchem/components/component_info.py
@@ -12,3 +12,5 @@ class ComponentInfo(BaseModel):
     owl_subclass_of: list[str] = [
         "http://purl.obolibrary.org/obo/OBI_0000968",
     ]  # 'device'
+    get_methods: dict[str, str] = dict()
+    put_methods: dict[str, str] = dict()
