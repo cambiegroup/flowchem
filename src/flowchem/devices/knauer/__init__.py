08b4f6591245720f8cb74a21c549f92c65515a11
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2024-09-23 16:08:48 +0200

    Remove auto-sample

--- src/flowchem/devices/knauer/__init__.py
+++ src/flowchem/devices/knauer/__init__.py
@@ -3,12 +3,10 @@ from .azura_compact import AzuraCompact
 from .dad import KnauerDAD
 from .knauer_finder import knauer_finder
 from .knauer_valve import KnauerValve
-from .knauer_autosampler import KnauerAutosampler
 
 __all__ = [
     "knauer_finder",
     "AzuraCompact",
     "KnauerDAD",
-    "KnauerValve",
-    "KnauerAutosampler"
+    "KnauerValve"
 ]
