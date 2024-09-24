d30bcf21d494b0b3b56e4d671ae236183d70178e
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2024-09-24 17:21:54 +0200

    kindly start file

--- src/flowchem/devices/fakedevice/fakedevice.py
+++ src/flowchem/devices/fakedevice/fakedevice.py
@@ -1,14 +1,14 @@
 from flowchem.devices.flowchem_device import FlowchemDevice
 from flowchem.components.device_info import DeviceInfo
-from flowchem.utils.people import Samuel_Saraiva
+from flowchem.utils.people import samuel_saraiva
 from flowchem.devices.fakedevice.fakedevice_component import FakeComponent_FakeDevice
 from loguru import logger
 
 class FakeDeviceExample(FlowchemDevice):
     """Our plugins fake device!"""
     device_info = DeviceInfo(
-        authors=[Samuel_Saraiva],
-        maintainers=[Samuel_Saraiva],
+        authors=[samuel_saraiva],
+        maintainers=[samuel_saraiva],
         manufacturer="Fake-device",
         model="FakeDevice",
         serial_number=42,
@@ -18,7 +18,7 @@ class FakeDeviceExample(FlowchemDevice):
     def __init__(self, name, another_attribute: str = "some_attribute"):
         super().__init__(name)
         self.device_info = DeviceInfo(
-            authors=[Samuel_Saraiva],
+            authors=[samuel_saraiva],
             manufacturer="FakeDeviceExample",
             model="Universal testing Actuator",
         )
