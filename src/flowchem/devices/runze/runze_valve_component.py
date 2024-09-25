1042727968a6b9c0faeef0ab165ad68f345105c7
magz97 <miguelgarciazubillaga@gmail.com>
2024-09-25 15:12:13 +0200

    Fixed mappings. In runze valves the positions start from the bottom and counter-clockwise.

--- src/flowchem/devices/runze/runze_valve_component.py
+++ src/flowchem/devices/runze/runze_valve_component.py
@@ -24,12 +24,13 @@ class Runze6PortDistributionValve(SixPortDistributionValve):
         self.add_api_route("/monitor_position", self.get_monitor_position, methods=["GET"])
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
-    def _change_connections(self, raw_position: str, reverse: bool = False):
-        raw_position = int(raw_position)
+    def _change_connections(self, raw_position: str | int, reverse: bool = False):
+        position_mapping = {0: 4, 1: 3, 2: 2, 3: 1, 4: 6, 5: 5}
         if reverse:
-            return raw_position - 1
+            reverse_mapping = {v: k for k, v in position_mapping.items()}
+            return reverse_mapping[raw_position]
         else:
-            return raw_position + 1
+            return position_mapping[raw_position]
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
@@ -50,12 +51,13 @@ class Runze8PortDistributionValve(EightPortDistributionValve):
         self.add_api_route("/monitor_position", self.get_monitor_position, methods=["GET"])
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
-    def _change_connections(self, raw_position: str, reverse: bool = False):
-        raw_position = int(raw_position)
+    def _change_connections(self, raw_position: str | int, reverse: bool = False):
+        position_mapping = {0: 5, 1: 4, 2: 3, 3: 2, 4: 1, 5: 8, 6: 7, 7: 6}
         if reverse:
-            return raw_position - 1
+            reverse_mapping = {v: k for k, v in position_mapping.items()}
+            return reverse_mapping[raw_position]
         else:
-            return raw_position + 1
+            return position_mapping[raw_position]
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
@@ -76,12 +78,13 @@ class Runze10PortDistributionValve(TenPortDistributionValve):
         self.add_api_route("/monitor_position", self.get_monitor_position, methods=["GET"])
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
-    def _change_connections(self, raw_position: str, reverse: bool = False):
-        raw_position = int(raw_position)
+    def _change_connections(self, raw_position: str | int, reverse: bool = False):
+        position_mapping = {0: 6, 1: 5, 2: 4, 3: 3, 4: 2, 5: 1, 6: 10, 7: 9, 8: 8, 9: 7}
         if reverse:
-            return raw_position - 1
+            reverse_mapping = {v: k for k, v in position_mapping.items()}
+            return reverse_mapping[raw_position]
         else:
-            return raw_position + 1
+            return position_mapping[raw_position]
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
@@ -102,12 +105,13 @@ class Runze12PortDistributionValve(TwelvePortDistributionValve):
         self.add_api_route("/monitor_position", self.get_monitor_position, methods=["GET"])
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
-    def _change_connections(self, raw_position: str, reverse: bool = False):
-        raw_position = int(raw_position)
+    def _change_connections(self, raw_position: str | int, reverse: bool = False):
+        position_mapping = {0: 7, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1, 7: 12, 8: 11, 9: 10, 10: 9, 11: 8}
         if reverse:
-            return raw_position - 1
+            reverse_mapping = {v: k for k, v in position_mapping.items()}
+            return reverse_mapping[raw_position]
         else:
-            return raw_position + 1
+            return position_mapping[raw_position]
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
@@ -128,12 +132,13 @@ class Runze16PortDistributionValve(SixteenPortDistributionValve):
         self.add_api_route("/monitor_position", self.get_monitor_position, methods=["GET"])
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
-    def _change_connections(self, raw_position: str, reverse: bool = False):
-        raw_position = int(raw_position)
+    def _change_connections(self, raw_position: str | int, reverse: bool = False):
+        position_mapping = {0: 9, 1: 8, 2: 7, 3: 6, 4: 5, 5: 4, 6: 3, 7: 2, 8: 1, 9: 16, 10: 15, 11: 14, 12: 13, 13: 12, 14: 11, 15: 10}
         if reverse:
-            return raw_position - 1
+            reverse_mapping = {v: k for k, v in position_mapping.items()}
+            return reverse_mapping[raw_position]
         else:
-            return raw_position + 1
+            return position_mapping[raw_position]
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
