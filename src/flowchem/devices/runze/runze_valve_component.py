c1b4850b4a6b73159b236ca83cf7927b81614729
magz97 <miguelgarciazubillaga@gmail.com>
2024-09-26 15:32:02 +0200

    Exchanged mapping for valves internal port numbers.

--- src/flowchem/devices/runze/runze_valve_component.py
+++ src/flowchem/devices/runze/runze_valve_component.py
@@ -25,12 +25,11 @@ class Runze6PortDistributionValve(SixPortDistributionValve):
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
     def _change_connections(self, raw_position: str | int, reverse: bool = False):
-        position_mapping = {0: 4, 1: 3, 2: 2, 3: 1, 4: 6, 5: 5}
+        anti_cc_stator = [4, 3, 2, 1, 6, 5]
         if reverse:
-            reverse_mapping = {v: k for k, v in position_mapping.items()}
-            return reverse_mapping[raw_position]
+            return anti_cc_stator[raw_position]
         else:
-            return position_mapping[raw_position]
+            return anti_cc_stator.index(raw_position)
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
@@ -52,12 +51,11 @@ class Runze8PortDistributionValve(EightPortDistributionValve):
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
     def _change_connections(self, raw_position: str | int, reverse: bool = False):
-        position_mapping = {0: 5, 1: 4, 2: 3, 3: 2, 4: 1, 5: 8, 6: 7, 7: 6}
+        anti_cc_stator = [5, 4, 3, 2, 1, 8, 7, 6]
         if reverse:
-            reverse_mapping = {v: k for k, v in position_mapping.items()}
-            return reverse_mapping[raw_position]
+            return anti_cc_stator[raw_position]
         else:
-            return position_mapping[raw_position]
+            return anti_cc_stator.index(raw_position)
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
@@ -79,12 +77,11 @@ class Runze10PortDistributionValve(TenPortDistributionValve):
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
     def _change_connections(self, raw_position: str | int, reverse: bool = False):
-        position_mapping = {0: 6, 1: 5, 2: 4, 3: 3, 4: 2, 5: 1, 6: 10, 7: 9, 8: 8, 9: 7}
+        anti_cc_stator = [6, 5, 4, 3, 2, 1, 10, 9, 8, 7]
         if reverse:
-            reverse_mapping = {v: k for k, v in position_mapping.items()}
-            return reverse_mapping[raw_position]
+            return anti_cc_stator[raw_position]
         else:
-            return position_mapping[raw_position]
+            return anti_cc_stator.index(raw_position)
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
@@ -106,12 +103,11 @@ class Runze12PortDistributionValve(TwelvePortDistributionValve):
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
     def _change_connections(self, raw_position: str | int, reverse: bool = False):
-        position_mapping = {0: 7, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1, 7: 12, 8: 11, 9: 10, 10: 9, 11: 8}
+        anti_cc_stator = [7, 6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8]
         if reverse:
-            reverse_mapping = {v: k for k, v in position_mapping.items()}
-            return reverse_mapping[raw_position]
+            return anti_cc_stator[raw_position]
         else:
-            return position_mapping[raw_position]
+            return anti_cc_stator.index(raw_position)
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
@@ -133,12 +129,11 @@ class Runze16PortDistributionValve(SixteenPortDistributionValve):
         self.add_api_route("/monitor_position", self.set_monitor_position, methods=["PUT"])
 
     def _change_connections(self, raw_position: str | int, reverse: bool = False):
-        position_mapping = {0: 9, 1: 8, 2: 7, 3: 6, 4: 5, 5: 4, 6: 3, 7: 2, 8: 1, 9: 16, 10: 15, 11: 14, 12: 13, 13: 12, 14: 11, 15: 10}
+        anti_cc_stator = [9, 8, 7, 6, 5, 4, 3, 2, 1, 16, 15, 14, 13, 12, 11, 10]
         if reverse:
-            reverse_mapping = {v: k for k, v in position_mapping.items()}
-            return reverse_mapping[raw_position]
+            return anti_cc_stator[raw_position]
         else:
-            return position_mapping[raw_position]
+            return anti_cc_stator.index(raw_position)
 
     async def get_monitor_position(self) -> str:
         """Get current valve position."""
