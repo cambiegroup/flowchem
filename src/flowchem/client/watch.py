8ed86e73e9bda2ef82850c04f7e6280407f0de47
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2024-10-04 17:58:47 +0200

    Watch class to manage logging, recording of specific devices, get functions, and set up the watchdog.

--- src/flowchem/client/watch.py
+++ src/flowchem/client/watch.py
@@ -1,143 +1,112 @@
 from flowchem.client.client import get_all_flowchem_devices
+from flowchem.client.device_client import FlowchemDeviceClient
+
+from datetime import datetime
 import time
 import asyncio
 import csv
 import os
+from pathlib import Path
 from loguru import logger
 import threading
 import toml
 import inspect
-from typing import Callable
+from typing import Callable, Any
 
 
 class Watch:
-    """
-    A class used to monitor and log the behavior of flowchem devices in real-time, recording data from devices
-    and components at a specified time interval. It also allows setting custom conditions (rules) to monitor
-    specific values from the devices and raise errors if the conditions are violated.
 
-    Attributes:
-    ----------
-    experimental_id : int | str
-        Unique identifier for the experiment being monitored.
+    def __init__(self, devices: dict[str, FlowchemDeviceClient],
+                 address: Path | str,
+                 experimental_id: str | int = 1,
+                 information_to_file: dict[str, Any] | None = None):
 
-    address : str
-        Directory path where logs and data will be saved, appended with experiment id.
+        self.devices = devices
 
-    discrete_time : float
-        Time interval in seconds at which data from the devices is logged.
-
-    add_info : dict | None
-        Additional information related to the experiment to be stored as a `.toml` file.
-
-    devices : dict
-        A dictionary of all flowchem devices obtained from the `get_all_flowchem_devices()` function.
-
-    files : dict
-        A dictionary to hold file handlers for writing CSV logs from devices.
-
-    loop : bool
-        A boolean flag controlling the logging loop in the background thread.
-
-    inspect : dict
-        A dictionary to store inspection rules (conditions) for specific devices, components, and commands.
-
-
-    Methods:
-    -------
-    __init__(experimental_id, address, discrete_time=1, add_info=None):
-        Initializes the Watch object with experiment id, logging address, and time interval for logging.
-
-    setup():
-        Sets up logging directories, initializes CSV files for devices and components, and stores additional
-        experiment information if provided.
-
-    run():
-        Starts a background thread that runs a logging loop, collecting data at specified intervals.
-
-    __run_loop():
-        Internal method that runs in a thread, periodically logging the values from the devices and comparing them
-        against custom rules if provided.
-
-    close():
-        Stops the logging loop, closes all open files, and terminates the background thread.
-
-    inset_inspect(device: str, component: str, command: str, condition: Callable):
-        Inserts an inspection rule for a specific device, component, and command, defining the condition that the
-        value must satisfy.
-
-    __rule_tranlate(value, rule) -> bool:
-        Translates and evaluates the received value against a provided rule, converting the value to a numeric
-        format if necessary. Returns True if the condition is satisfied, or logs an error if not.
-    """
-
-    def __init__(self,
-                 experimental_id: int | str,
-                 address: str,
-                 discrete_time: float = 1,
-                 add_info: dict | None = None):
-        """
-        Initializes the Watch instance with an experiment id, a logging directory, a time interval,
-        and optional additional information to store.
-
-        Args:
-            experimental_id: Unique identifier for the experiment (can be an integer or a string).
-            address: Directory where experiment logs and data will be stored.
-            discrete_time: Time interval (in seconds) at which device data will be logged. Default is 1 second.
-            add_info: Optional dictionary containing additional experiment information to save as a `.toml` file.
-        """
-        self.devices = get_all_flowchem_devices(IP_machine="local")
-        self.files = dict()
-        self.loop = True
         self.experimental_id = experimental_id
-        self.discrete_time = discrete_time
-        self.address = address + f"_experiment_id_{self.experimental_id}"
-        self.add_info = add_info
-        self.inspect = dict()
 
-    def setup(self):
+        self.add_info = information_to_file
+
+        self.loop = True
+
+        self.file: dict[str, Any] = dict()
+
+        self.address = os.path.join(address,f"_experiment_id_{self.experimental_id}")
+
+        self.inspect: dict[str, Any] = dict()
+
+        self.start_watch = time.localtime()
+
+        self.devices_to_watch: list[str] = None
+
+        self.discrete_time = 2
+
+        if not os.path.exists(self.address):
+            os.mkdir(self.address)
+            logger.add(self.address + "/Watchlog.log", level="INFO")
+        else:
+            logger.add(self.address + "/Watchlog.log", level="INFO")
+            logger.info(f"Recording folder {self.address} already exists. The supervisor will overwrite it")
+
+        if self.add_info is not None:
+            with open(self.address + f"/Information_id_{self.experimental_id}.toml", "w") as file:
+                toml.dump(self.add_info, file)
+
+    def setup_watch_client(self,
+                           devices_to_watch: list[str] | dict[str, FlowchemDeviceClient],
+                           discrete_time: float = 2.0):
         """
         Sets up the environment for logging by creating directories for storing data and logs for each
         device and component. Also saves additional information if provided in the `add_info` attribute.
 
         This method initializes CSV files where component data is written during the logging loop.
         """
-        try:
-            os.mkdir(self.address)
-        except:
-            logger.info(f"Recording folder {self.address} already exists. The supervisor will overwrite it")
 
-        logger.add(self.address + "/Watchlog.log", level="INFO")
+        if type(devices_to_watch) == dict:
 
-        if self.add_info is not None:
-            with open(self.address + f"/Information_id_{self.experimental_id}.toml", "w") as file:
-                toml.dump(self.add_info, file)
+            devices_to_watch = [d for d in devices_to_watch.keys()]
 
-        for device_name, device in self.devices.items():
-            try:
-                os.mkdir(self.address + "/" + device_name)
-            except:
-                logger.info(f"Recording device folder {device_name} already exists. The supervisor will overwrite it")
+        self.devices_to_watch = devices_to_watch
 
-            logger.info(f"Recording folder from device: {device_name} was created")
+        self.discrete_time = discrete_time
 
-            self.files[device_name] = dict()
+        if self.devices_to_watch:
 
-            for component_name, component in device.components.items():
-                self.files[device_name][component_name] = dict()
+            row_name = ["time"]
 
-                for method in component.component_info.get_methods.keys():
-                    file = open(f"{self.address}/{device_name}/{component_name}_{method}.csv", mode="w", newline="")
-                    writer = csv.DictWriter(file, fieldnames=["time", "value"])
-                    writer.writeheader()
-                    self.files[device_name][component_name][method] = [writer, file]
+            for device_name in self.devices_to_watch:
+
+                for component in self.devices[device_name].components.values():
+
+                    component_name = component.component_info.name
+
+                    for method in component.component_info.get_methods.keys():
+
+                        row_name.append(f"{device_name}/{component_name}/{method}")
+
+            file = open(f"{self.address}/data.csv", mode="w", newline="")
+
+            writer = csv.DictWriter(file, fieldnames=row_name)
+
+            writer.writeheader()
+
+            self.file = {"write": writer, "file": file}
+
+            logger.info(f"Recording of the devices: {self.devices_to_watch} was setup")
 
     def run(self):
         """
         Starts the data logging process by launching a background thread that periodically logs device and
         component data at the time interval specified by `discrete_time`.
         """
-        threading.Thread(target=self.__run_loop).start()
+        if self.devices_to_watch is not None:
+
+            threading.Thread(target=self.__run_loop).start()
+
+        else:
+
+            logger.warning(f"There is not device to watch. Be sure that the devices were setup in the "
+                           f"class initialization: Watch(...,devices_towatch=[...]")
 
     def __run_loop(self):
         """
@@ -148,44 +117,43 @@ class Watch:
         toc = time.perf_counter()
 
         while self.loop:
-            current_time = time.localtime()
-            year, month, day = current_time.tm_year, current_time.tm_mon, current_time.tm_mday
-            hour, minute, second = current_time.tm_hour, current_time.tm_min, current_time.tm_sec
 
             if time.perf_counter() - toc >= self.discrete_time:
-                for device_name, device in self.devices.items():
-                    for component_name, component in device.components.items():
+                row = dict()
+                for device_name in self.devices_to_watch:
+
+                    for component in self.devices[device_name].components.values():
+
+                        component_name = component.component_info.name
+
                         for method in component.component_info.get_methods.keys():
+
+                            current_time = time.localtime()
+
                             value = self.devices[device_name][component_name].get(method).text
-                            row = {
-                                "time": f"{year}-{month:02d}-{day:02d}-{hour:02d}:{minute:02d}:{second:02d}",
-                                "value": value
-                            }
-                            self.files[device_name][component_name][method][0].writerow(row)
+
+                            year, month, day = current_time.tm_year, current_time.tm_mon, current_time.tm_mday
+                            hour, minute, second = current_time.tm_hour, current_time.tm_min, current_time.tm_sec
+
+                            row["time"] = f"{year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d}"
+
+                            row[f"{device_name}/{component.component_info.name}/{method}"] = value
 
                             if device_name in self.inspect.keys():
+
                                 if component_name in self.inspect[device_name].keys():
+
                                     if method in self.inspect[device_name][component_name].keys():
+
                                         rule = self.inspect[device_name][component_name][method]
+
                                         if not self.__rule_tranlate(value, rule):
                                             logger.error(f"The {method} of the component/device: {component_name}/"
                                                          f"{device_name} should obey the rule: {inspect.getsource(rule)},"
                                                          f" however it returned: {value}")
+                self.file["write"].writerow(row)
 
                 toc = time.perf_counter()
-
-    def close(self):
-        """
-        Stops the logging process, terminates the background thread, and closes all open CSV files
-        where device data was being logged.
-        """
-        self.loop = False
-        time.sleep(0.5)
-
-        for device_name, device in self.devices.items():
-            for component_name, component in device.components.items():
-                for method in component.component_info.get_methods.keys():
-                    self.files[device_name][component_name][method][1].close()
 
     def inset_inspect(self, device: str, component: str, command: str, condition: Callable):
         """
@@ -229,16 +197,70 @@ class Watch:
                                  f"{inspect.getsource(rule)}")
                     return True
 
+    def close(self):
+        """
+        Stops the logging process, terminates the background thread, and closes all open CSV files
+        where device data was being logged.
+        """
+
+        """ Getting the current logs """
+        # Convert time.localtime() to a datetime object
+        specific_time = datetime(*self.start_watch[:6])
+
+        logging_dir = os.path.join(os.path.dirname(__file__), "loggings")
+
+        components_logs = dict()
+
+        lines = []
+
+        for devices_name in os.listdir(logging_dir):
+
+            subdirectories = os.path.join(logging_dir,devices_name)
+
+            for f in os.listdir(subdirectories):
+
+                # Open the log file and filter the lines based on the target time
+                with open(os.path.join(subdirectories, f), 'r') as log_file:
+                    for line in log_file:
+                        # Extract the timestamp part of the log line (assuming it's the first part)
+                        timestamp_str = line.split('-Endpoint')[0]
+                        log_time = datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S.%f%z').replace(tzinfo=None)
+
+                        # Compare the log time with the specific time
+                        if log_time >= specific_time:
+
+                            lines.append((log_time, line))
+
+        # Sort the list of log lines by the log_time
+        sorted_lines = sorted(lines, key=lambda x: x[0])
+
+        # Write the sorted lines into the output file
+        with open(os.path.join(self.address, "loggings.log"), 'w') as output:
+            for _, line in sorted_lines:
+                output.write(line)
+
+        if self.devices_to_watch is not None:
+            if self.loop:
+                self.loop = False
+            time.sleep(0.5)
+            self.file["file"].close()
+
+
 
 if __name__ == "__main__":
 
     address = os.path.dirname(os.path.abspath(__file__))
 
-    inf = {"some_information": "oiiuasd4712", "id_io": 8587}
+    devices = get_all_flowchem_devices(IP_machine="local")
 
-    watch = Watch(experimental_id=1, address=address + "/recording", discrete_time=2, add_info=inf)
+    inf = {"some_information": "---", "id_io": 0000}
 
-    watch.setup()
+    watch = Watch(devices=devices,
+                  address=address,
+                  information_to_file=inf,
+                  experimental_id=0)
+
+    watch.setup_watch_client(devices_to_watch=devices)
 
     watch.inset_inspect(device="fake-device",
                         component="FakeComponent",
