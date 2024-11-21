e89e471d837418c1f357518882e7e244c3fee1c0
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2024-11-21 14:35:12 +0100

    fix mypy error:
    Trying with Tuple of typing!

--- src/flowchem/components/valves/valve.py
+++ src/flowchem/components/valves/valve.py
@@ -67,7 +67,7 @@ def all_tuples_in_nested_tuple(tuple_in: Tuple[Tuple[int, int], ...],
 
 
 def no_tuple_in_nested_tuple(tuple_in: Tuple[Tuple[int, int], ...],
-                             tuple_contains: Tuple[tuple[int, int, ...], ...]) -> bool:
+                             tuple_contains: Tuple[Tuple[int, int, ...], ...]) -> bool:
     """Check if none of requested tuples are in a tuple of tuples"""
     contains_tuple = False
     for subtuple in tuple_in:
@@ -80,7 +80,7 @@ def no_tuple_in_nested_tuple(tuple_in: Tuple[Tuple[int, int], ...],
 class Valve(FlowchemComponent):
     """An abstract class for devices of type valve.
 
-    .. warning::
+    ... warning::
         Device objects should not directly generate components with this object but rather a more specific valve type,
         such as `SixPortTwoPositionValve` or `SixPortPositionValve`.
 
@@ -111,7 +111,8 @@ class Valve(FlowchemComponent):
             rotor and stator are both represented like:
             (   (1,2,3,4,5,6),          (0))
                 radial ports       middle ports
-            Ports should be equally distributed, with equally spaced angle in between. If this is not the case, add None
+                          should be equally distributed, with equally spaced angle in between. If this is not the
+                          case, add None
              for missing port
 
         """
@@ -128,7 +129,8 @@ class Valve(FlowchemComponent):
         self.add_api_route("/position", self.set_position, methods=["PUT"])
         self.add_api_route("/connections", self.connections, methods=["GET"])
 
-    def _create_connections(self, stator_ports, rotor_ports):
+    @staticmethod
+    def _create_connections(stator_ports, rotor_ports):
         """
         Create possible switching states from a stator and rotor representation. Position names are integers. Going to
         the next position in clockwise direction increases position name by one
@@ -139,7 +141,7 @@ class Valve(FlowchemComponent):
         if len(rotor_ports) == 1:
             # in case there is no 0 port, for data uniformity, internally add it. strictly, the stator and rotor
             # should reflect physical properties, so if stator has a hole in middle it should have 0, but only rotor
-            # None. Sinc ethis does not impact functionality, thoroughness will be left to the user
+            # None. Sinc this does not impact functionality, thoroughness will be left to the user
             rotor_ports.append([None])
             stator_ports.append([None])
         # it is rather simple: we just move the rotor by one and thereby create a dictionary
@@ -173,15 +175,15 @@ class Valve(FlowchemComponent):
 
         return connections
 
-    def _change_connections(self, raw_position: int | str, reverse: bool = False) -> int | str:
-        # abstract valve mapping needs to be translated to device-specific position naming. This can be eg
+    def _change_connections(self, raw_position: int | str, reverse: bool = False):
+        # abstract valve mapping needs to be translated to device-specific position naming. This can be e.g.
         # addition/subtraction of one, multiplication with some angle or mapping to letters. Needs to be implemented on
         # device level since this is device communication protocol specific
         raise NotImplementedError
 
     def _connect_positions(self,
-                           positions_to_connect: tuple[tuple],
-                           positions_not_to_connect: tuple[tuple] | None = None,
+                           positions_to_connect: Tuple[Tuple[int, int], ...],
+                           positions_not_to_connect: Tuple[Tuple[int, int], ...] | None = None,
                            arbitrary_switching: bool = True) -> int:
         """
         This is the heart of valve switching logic: select the suitable position (so actually the key in
@@ -225,17 +227,17 @@ class Valve(FlowchemComponent):
                            disconnect: str = "",
                            ambiguous_switching: str | bool = False):
         """Move valve to position, which connects named ports"""
-        connect_tuple=return_tuple_from_input(connect)
-        disconnect_tuple=return_tuple_from_input(disconnect)
-        ambiguous_switching_bool=return_bool_from_input(ambiguous_switching)
-        target_pos = self._connect_positions(positions_to_connect=connect_tuple, positions_not_to_connect=disconnect_tuple,
+        connect_tuple = return_tuple_from_input(connect)
+        disconnect_tuple = return_tuple_from_input(disconnect)
+        ambiguous_switching_bool = return_bool_from_input(ambiguous_switching)
+        target_pos = self._connect_positions(positions_to_connect=connect_tuple,
+                                             positions_not_to_connect=disconnect_tuple,
                                              arbitrary_switching=ambiguous_switching_bool)
         target_pos = self._change_connections(target_pos)
         if not hasattr(self, "identifier"):
             await self.hw_device.set_raw_position(target_pos)
         else:
             await self.hw_device.set_raw_position(target_pos, target_component=self.identifier)
-
 
     def connections(self) -> ValveInfo:
         """Get the list of all available positions for this valve.
@@ -244,20 +246,22 @@ class Valve(FlowchemComponent):
         return ValveInfo(ports=self._stator_ports, positions=self._positions)
 
     # Philosophy: explicitly specify which ports to connect
-    # In case of a simple multiposition valve, it always connects the always open central port to the requested port.
-    # But there are more complex rotors available, already with an injection valve: connect((1, 2)) is concise and clear.
+    # In case of a simple multi-position valve, it always connects the always open central port to the requested port.
+    # But there are more complex rotors available, already with an injection valve: connect((1, 2)) is concise and
+    # clear.
     # What is even more important, one can specify which ports to not connect (optionally during switching)
-    # This issue is most pressing with hamilton valves, where some positions connect 3 ports and it is very hard to
+    # This issue is most pressing with hamilton valves, where some positions connect 3 ports, and it is very hard to
     # foresee what command does what. SO here a simple connect((1,2)) helps.
     # In order for that to work, and to make the coding and usage simple and concise, some definitions are needed:
     # 1) The port zero can exist, but does not necessarily.
-    #    For nomenclature reasons, port zero is the one the turning axis and only this one. COmmonly, this port,
+    #    For nomenclature reasons, port zero is the one the turning axis and only this one. Commonly, this port,
     #    if existing, is always open
-    # 2) At the physical valve, the upmost is port 1
-    #   a) If there is no port straight on top, then one goes in clockwise direction, until a port comes, which is then one
+    # 2) At the physical valve, the upmost is port 1.
+    # a) If there is no port straight on top, then one goes in clockwise direction, until a port comes, which is then
+    # one
     # 3 )Beware: For logical reasons, we need to introduce ports of "number" None. These are needed because we need to
     #   define dead-ends. These dead-ends are IMMUTABLE dead-ends, so the stator or rotor do not have an opening there
-    #   Any time there is a different amount of positions on rotor and stator, Noneports are introduced
+    #   Any time there is a different amount of positions on rotor and stator, None ports are introduced
     # 5) Mutable dead-ends: blanking plugs are treated as port number, the consumer needs to deal with its definition by
     #   graph or similar since blanking plugs on valve side could be open
     # Dead-ends are needed because we represent valves as graphs, edges are represented by same numbers shared. If a
@@ -265,5 +269,5 @@ class Valve(FlowchemComponent):
     # to work: Again the hamilton, it will become clear why. So much now: The rotor has more open positions than the
     # stator.
     # 6) The so far mentioned logic only strictly applies to valves facing the user with their front side, however,
-    # e.g. the autosampler faces one valve with its always open port to the ground. Simply flip horizontally until it
+    # e.g. the auto-sampler faces one valve with its always open port to the ground. Simply flip horizontally until it
     # faces you
