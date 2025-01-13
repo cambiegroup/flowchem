d1c1ebb9c18b9caed735e032ecadd424d7f50bb9
magz97 <miguelgarciazubillaga@gmail.com>
2025-01-13 16:38:35 +0100

    fixed __init__ of knauer

--- src/flowchem/devices/knauer/__init__.py
+++ src/flowchem/devices/knauer/__init__.py
@@ -3,6 +3,7 @@ from .azura_compact import AzuraCompact
 from .dad import KnauerDAD
 from .knauer_finder import knauer_finder
 from .knauer_valve import KnauerValve
+from .knauer_autosampler import KnauerAutosampler
 
 __all__ = [
     "knauer_finder",
