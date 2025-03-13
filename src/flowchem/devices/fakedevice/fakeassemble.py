25d9e99b5045fbc323c8fe5bc0bee590a1f36fcc
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2025-03-13 17:46:53 +0100

    Incorporate all components in FakeAssemble, only for test it.

--- src/flowchem/devices/fakedevice/fakeassemble.py
+++ src/flowchem/devices/fakedevice/fakeassemble.py
@@ -1,7 +1,7 @@
 from flowchem.devices.flowchem_device import FlowchemDevice
 from flowchem.components.device_info import DeviceInfo
 from flowchem.utils.people import samuel_saraiva
-from flowchem.devices.fakedevice.fakeassemble_components import FakePump, FakeValvePosition, FakeValveDistribution, FakePressureSensor
+from flowchem.devices.fakedevice.fakeassemble_components import *
 
 from loguru import logger
 
@@ -26,10 +26,25 @@ class FakeAssemble(FlowchemDevice):
 
     async def initialize(self):
         logger.info('FakeDevice devices was successfully initialized!')
-        self.components.extend([FakePump("pump", self)])
+
+        self.components.extend([FakeHPLC("hplc", self)])
+        self.components.extend([FakeIRC("ir", self)])
+        self.components.extend([FakeNMR("nmr", self)])
+
+        self.components.extend([FakeHPLCPump("pump_hplc", self)])
+        self.components.extend([FakePump("syringe", self)])
+
+        self.components.extend([FakePhotoSensor("photosensor", self)])
+        self.components.extend([FakePressureSensor("pressure", self)])
+
+        self.components.extend([FakePhotoreactor("photoreactor", self)])
+        self.components.extend([FakePressureControl("pressure_control", self)])
+        self.components.extend([FakePowerControl("power", self)])
+        self.components.extend([FakeTemperatureControl("temperature", self,
+                                                       TempRange(min="0 °C", max="100 °C"))])
+
         self.components.extend([FakeValvePosition("valve_p", self)])
         self.components.extend([FakeValveDistribution("valve_d", self)])
-        self.components.extend([FakePressureSensor("sensor", self)])
 
     async def set_raw_position(self, target_pos, target_component: str = "distribution"):
         self.raw_position[target_component] = target_pos
@@ -45,9 +60,9 @@ if __name__ == '__main__':
     asyncio.run(device.initialize())
 
     async def interaction():
-        pos = await device.components[2].get_position()
+        pos = await device.components[-1].get_position()
         print(pos)
-        await device.components[2].set_position("[[3, 0]]")
+        await device.components[-1].set_position("[[3, 0]]")
     asyncio.run(interaction())
 
 
--- src/flowchem/devices/fakedevice/fakeassemble_components.py
+++ src/flowchem/devices/fakedevice/fakeassemble_components.py
@@ -1,14 +1,52 @@
+import pint
+from flowchem.components.analytics.hplc import HPLCControl
+from flowchem.components.analytics.ir import IRControl, IRSpectrum
+from flowchem.components.analytics.nmr import NMRControl
+
 from flowchem.components.pumps.syringe_pump import SyringePump
+from flowchem.components.pumps.hplc_pump import HPLCPump
+
+from flowchem.components.sensors.photo_sensor import PhotoSensor
+from flowchem.components.sensors.pressure_sensor import PressureSensor
+
+from flowchem.components.technical.photo import Photoreactor
+from flowchem.components.technical.pressure import PressureControl
+from flowchem.components.technical.power import PowerControl
+from flowchem.components.technical.temperature import TemperatureControl, TempRange
+
 from flowchem.components.valves.distribution_valves import SixPortDistributionValve
 from flowchem.components.valves.injection_valves import SixPortTwoPositionValve
-from flowchem.components.sensors.pressure_sensor import PressureSensor
+
 from flowchem.devices.flowchem_device import FlowchemDevice
+
+""" Analytics """
+
+class FakeHPLC(HPLCControl):
+
+    async def send_method(self, method_name):
+        return True
+
+    async def run_sample(self, sample_name: str, method_name: str):
+        return True
+
+
+class FakeIRC(IRControl):
+
+    async def acquire_spectrum(self) -> IRSpectrum:
+        return IRSpectrum()
+
+
+class FakeNMR(NMRControl):
+    ...
+
+
+""" Pumps """
+
+class FakeHPLCPump(HPLCPump):
+    ...
 
 
 class FakePump(SyringePump):
-
-    def __init__(self, name: str, hw_device: FlowchemDevice):
-        super().__init__(name, hw_device)
 
     async def infuse(self, rate: str = "", volume: str = "") -> bool:  # type: ignore
         """Start infusion."""
@@ -31,6 +69,55 @@ class FakePump(SyringePump):
         """Pump in the opposite direction of infuse."""
         return True
 
+
+""" Sensors """
+
+class FakePhotoSensor(PhotoSensor):
+    ...
+
+
+class FakePressureSensor(PressureSensor):
+
+    async def read_pressure(self, units: str = "bar") -> bool:
+        """
+        Read the current pressure from the sensor and return it in the specified units.
+
+        Parameters:
+        -----------
+        units : str, optional
+            The units in which to return the pressure (default is bar).
+
+        Returns:
+        --------
+        float
+            The current pressure in the specified units.
+        """
+        return 0.5
+
+
+""" Technical """
+
+class FakePhotoreactor(Photoreactor):
+    ...
+
+
+class FakePressureControl(PressureControl):
+
+    async def set_pressure(self, pressure: str):
+        ...
+
+
+class FakePowerControl(PowerControl):
+    ...
+
+
+class FakeTemperatureControl(TemperatureControl):
+
+    async def set_temperature(self, temp: str):
+        ...
+
+
+""" Valves """
 
 class FakeValveDistribution(SixPortDistributionValve):
 
@@ -57,26 +144,5 @@ class FakeValvePosition(SixPortTwoPositionValve):
         else:
             return raw_position + 1
 
-
-class FakePressureSensor(PressureSensor):
-
-    def __init__(self, name: str, hw_device: FlowchemDevice):
-        super().__init__(name, hw_device)
-
-    async def read_pressure(self, units: str = "bar") -> bool:
-        """
-        Read the current pressure from the sensor and return it in the specified units.
-
-        Parameters:
-        -----------
-        units : str, optional
-            The units in which to return the pressure (default is bar).
-
-        Returns:
-        --------
-        float
-            The current pressure in the specified units.
-        """
-        return 0.5
 
 
