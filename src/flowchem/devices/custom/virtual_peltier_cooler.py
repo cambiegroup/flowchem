30f81149f9df73413979683d810c8407f5bb6ed1
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2025-04-11 11:57:17 +0200

    Update virtual_peltier_cooler.py
    Simplify the Peltier virtual device approach.

--- src/flowchem/devices/custom/virtual_peltier_cooler.py
+++ src/flowchem/devices/custom/virtual_peltier_cooler.py
@@ -1,103 +1,43 @@
-from flowchem.components.device_info import DeviceInfo
-from flowchem.devices.custom.peltier_cooler import PeltierCooler, PeltierIO, PeltierCommand, PeltierCommands
+from flowchem.components.technical.temperature import TempRange
+from flowchem.devices.flowchem_device import FlowchemDevice
+from flowchem.devices.custom.peltier_cooler_component import (
+    PeltierCoolerTemperatureControl,
+)
 from flowchem.utils.people import samuel_saraiva
+from flowchem import ureg
 from loguru import logger
 import asyncio
 import pint
 
 
-class Serial:
-    port = "Fake"
+class VirtualPeltierCooler(FlowchemDevice):
 
+    def __init__(self, name: str = "", *args, **kwargs):
 
-class VirtualPeltierIO(PeltierIO):
+        super().__init__(name)
 
-    def __init__(self):
-        # Initialize the parent class with a dummy serial port
-
-        # Simulated state variables
-        self._temperature = 25.0  # Simulated temperature in °C
-        self._sink_temperature = 25.0  # Simulated sink temperature in °C
-        self._power = 0.0  # Simulated power in W
-        self._current = 0  # Simulated current in A
-        self._is_on = False  # Simulated state (on/off)
-        self._setpoint = 25.0  # Simulated setpoint temperature in °C
-        self._serial = Serial()
-
-    @classmethod
-    def from_config(cls, port, **serial_kwargs):
-        return cls()
-
-    async def _write(self, command: PeltierCommand):
-        command_compiled = command.compile()
-        logger.debug(f"Sending virtual command: {repr(command_compiled)}")
-
-    async def _read_reply(self, command: PeltierCommand) -> str:
-        """Simulate reading a reply from the virtual device."""
-        logger.debug(f"Virtual Peltier generating reply for command: {command.compile()}")
-
-        if command.command_string == "GT1":  # Get temperature
-            return f"{self._temperature}"
-        elif command.command_string == "GT2":  # Get sink temperature
-            return f"{self._sink_temperature}"
-        elif command.command_string == "STV":  # Set temperature
-            self._setpoint = float(command.command_argument) / 100
-            return f"{self._setpoint}"
-        elif command.command_string == "SEN":  # Switch on
-            self._is_on = True
-            return "1"
-        elif command.command_string == "SDI":  # Switch off
-            self._is_on = False
-            return "0"
-        elif command.command_string == "GCU":  # Get power
-            return f"{self._power}"
-        elif command.command_string == "GPW":  # Get current
-            return f"{self._current}"
-        else:
-            return "0"
-
-    async def write_and_read_reply(self, command: PeltierCommand) -> str:
-        """Simulate sending a command and reading the reply."""
-        await self._write(command)
-        response = await self._read_reply(command)
-        return response
-
-
-class VirtualPeltierCooler(PeltierCooler):
-
-    def __init__(self,
-                 peltier_io: PeltierIO,
-                 name: str = "",
-                 address: int = 0,
-                 peltier_defaults: str | None = None):
-
-        super().__init__(peltier_io=peltier_io,
-                         name=name,
-                         address=address,
-                         peltier_defaults=peltier_defaults)
-
-        self.device_info = DeviceInfo(
-            authors=[samuel_saraiva],
-            manufacturer="Virtual Manufacturer",
-            model="Virtual Peltier Cooler",
-        )
+        self.device_info.authors = [samuel_saraiva]
+        self.device_info.manufacturer = "Virtual Manufacturer"
+        self.device_info.model = "Virtual Peltier Cooler"
+        logger.debug(f"Connected virtual Virtual Peltier Cooler")
 
         self.current_temperature = 0
         self.power = 0
 
-    @classmethod
-    def from_config(
-            cls,
-            port: str,
-            address: int,
-            name: str = "",
-            peltier_defaults: str | None = None,
-            **serial_kwargs,
-    ):
+    async def initialize(self):
+        temp_range = TempRange(
+            min=ureg.Quantity(f"0 °C"),
+            max=ureg.Quantity(f"100 °C")
+        )
+        list_of_components = [
+            PeltierCoolerTemperatureControl("temperature_control", self, temp_limits=temp_range)
+        ]
+        self.components.extend(list_of_components)
 
-        peltier_io = VirtualPeltierIO.from_config(port, **serial_kwargs)
-        logger.info(f"Connected to virtual Peltier Cooler: {name} at address {address}")
-        return cls(peltier_io=peltier_io, address=address, name=name, peltier_defaults=peltier_defaults)
+    @classmethod
+    def from_config(cls, *args, name: str = "", **kwargs):
+        logger.info(f"Connected to virtual Peltier Cooler: {name}")
+        return cls(name=name)
 
     async def set_temperature(self, temperature: pint.Quantity):
         """ Override set_temperature to simulate temperature changes. """
@@ -113,52 +53,11 @@ class VirtualPeltierCooler(PeltierCooler):
         """ Override get_power to return a simulated power value. """
         return self.power
 
-    async def _set_current_limit_cooling(self, current_limit: float):
-        # current in amp
-        await self.send_command_and_read_reply(PeltierCommands.COOLING_CURRENT_LIMIT, round(current_limit * 100))
+    async def start_control(self):
+        ...
 
-    async def _set_current_limit_heating(self, current_limit: float):
-        # current in amp
-        await self.send_command_and_read_reply(PeltierCommands.HEATING_CURRENT_LIMIT, round(current_limit * 100))
+    async def stop_control(self):
+        ...
 
-    async def _set_d_of_pid(self, differential: float):
-        # max 10
-        await self.send_command_and_read_reply(PeltierCommands.SET_DIFFERENTIAL_PID, round(differential * 100))
-
-    async def _set_i_of_pid(self, integral):
-        # max 10
-        await self.send_command_and_read_reply(PeltierCommands.SET_INTEGRAL_PID, round(integral * 100))
-
-    async def _set_p_of_pid(self, proportional):
-        # max 10
-        await self.send_command_and_read_reply(PeltierCommands.SET_PROPORTIONAL_PID, round(proportional * 100))
-
-    async def _set_max_temperature(self, t_max):
-        # max 10
-        await self.send_command_and_read_reply(PeltierCommands.SET_T_MAX, round(t_max * 100))
-
-    async def _set_min_temperature(self, t_min):
-        # max 10
-        await self.send_command_and_read_reply(PeltierCommands.SET_T_MIN, round(t_min * 100))
-
-
-async def main():
-    from flowchem import ureg
-    # Create a virtual PeltierCooler instance
-    virtual_peltier = VirtualPeltierCooler.from_config(port="COMX", name="Virtual Peltier", address=0)
-
-    # Initialize the virtual device
-    await virtual_peltier.initialize()
-
-    # Set a temperature and read it back
-    await virtual_peltier.set_temperature(ureg.Quantity("10 °C"))
-    current_temp = await virtual_peltier.get_temperature()
-    print(f"Current temperature: {current_temp} °C")
-
-    # Turn off the virtual device
-    await virtual_peltier.stop_control()
-
-
-if __name__ == "__main__":
-    # Run the main function
-    asyncio.run(main())
+    async def get_parameters(self) -> str:
+        return f"{self.current_temperature},1"
