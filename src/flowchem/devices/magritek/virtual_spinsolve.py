55fee371c26a9a53810915de434bbc2bb64b5572
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2025-04-16 13:41:18 +0200

    Simplify virtual Manson Power

--- src/flowchem/devices/magritek/virtual_spinsolve.py
+++ src/flowchem/devices/magritek/virtual_spinsolve.py
@@ -1,12 +1,20 @@
-from .spinsolve import Spinsolve, SpinsolveControl
+from flowchem.devices.flowchem_device import FlowchemDevice
+from flowchem.devices.magritek.spinsolve_control import SpinsolveControl
+from flowchem.utils.people import samuel_saraiva
 from fastapi import BackgroundTasks
 
 
-class VirtualSpinsolve(Spinsolve):
+class VirtualSpinsolve(FlowchemDevice):
+
+    def __init__(self, name: str | None = None, **kwargs) -> None:
+        """Control a Spinsolve instance via HTTP XML API."""
+        super().__init__(name)
+        self.device_info.version = "Virtual Spinsolve"
+        self.device_info.model = "Virtual Spectrometer"
+        self.device_info.authors = [samuel_saraiva]
 
     async def initialize(self):
-
-        self.components.append(SpinsolveControl("nmr-control", self))
+        self.components.append(SpinsolveControl("nmr-control", self))  # typo: ignore
 
     async def get_sample(self) -> str:
         return ""
@@ -25,6 +33,16 @@ class VirtualSpinsolve(Spinsolve):
 
     async def set_user_data(self, data: dict):
         return {}
+
+    async def list_protocols(self):
+        return []
+
+    async def get_result_folder(self, result_id: int | None = None):
+        return ""
+
+    async def is_protocol_running(self) -> bool:
+        return True
+
 
     async def run_protocol(
         self,
