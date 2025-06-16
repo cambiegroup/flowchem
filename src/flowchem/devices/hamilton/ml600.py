37f56155db6a737efeeb0cd0622ac5c0a2c0fa30
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2025-06-16 17:58:05 +0200

    mypy complains!

--- src/flowchem/devices/hamilton/ml600.py
+++ src/flowchem/devices/hamilton/ml600.py
@@ -359,19 +359,19 @@ class ML600(FlowchemDevice):
 
     def inspect_valve_argument(self, config: dict):
         if config.get("valve_left_class") and config.get("valve_left_class") not in ValveType:
-            logger.error(f"Invalid valve configuration in left valve of {self.name}! "
-                         f"Supported valve types are: {[v.value for v in ValveType]}. Assuming "
-                         f"{ML600.DEFAULT_CONFIG["valve_left_class"]}!")
+            logger.error(f"Invalid valve configuration in left valve of {self.name}!")
+            logger.error(f"Supported valve types are: {[v.value for v in ValveType]}.")
+            logger.error(f"Assuming {ML600.DEFAULT_CONFIG["valve_left_class"]}!")
             config.pop("valve_left_class")
         if config.get("valve_rigth_class") and config.get("valve_rigth_class") not in ValveType:
-            logger.error(f"Invalid valve configuration in rigth valve of {self.name}! "
-                         f"Supported valve types are: {[v.value for v in ValveType]}. Assuming "
-                         f"{ML600.DEFAULT_CONFIG["valve_rigth_class"]}!")
+            logger.error(f"Invalid valve configuration in rigth valve of {self.name}!")
+            logger.error(f"Supported valve types are: {[v.value for v in ValveType]}.")
+            logger.error(f"Assuming {ML600.DEFAULT_CONFIG["valve_rigth_class"]}!")
             config.pop("valve_rigth_class")
         if config.get("valve_class") and config.get("valve_class") not in ValveType:
-            logger.error(f"Invalid valve configuration in valve of {self.name}! "
-                         f"Supported valve types are: {[v.value for v in ValveType]}. Assuming "
-                         f"{ML600.DEFAULT_CONFIG["valve_class"]}!")
+            logger.error(f"Invalid valve configuration in valve of {self.name}!")
+            logger.error(f"Supported valve types are: {[v.value for v in ValveType]}.")
+            logger.error(f"Assuming {ML600.DEFAULT_CONFIG["valve_class"]}!")
             config.pop("valve_class")
         # This will merger the config into ML600.DEFAULT_CONFIG (in order to update)
         self.config = ML600.DEFAULT_CONFIG | config
